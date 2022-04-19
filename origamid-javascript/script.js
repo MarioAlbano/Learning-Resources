596b67b69aa7c1d79b8775b9124c1523b4940964
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-19 12:49:17 -0300

    test: exercise js random number

--- origamid-javascript/script.js
+++ origamid-javascript/script.js
@@ -1,9 +1,25 @@
-const filme = {
-    nome: 'O Senhor dos Anéis',
-    ano: 2001,
-    diretor: 'Peter Jackson',
-  };
+var n1 = parseFloat(prompt("Número do jogador 1:"));
+var n2 = parseFloat(prompt("Número do jogador 2:"));
+sorteio = parseInt(Math.random() * 2);
 
-  console.log(filme);
-console.log(filme.diretor);
+alert("O resultado do número sorteado é: " + sorteio);
 
+if (n1 === n2) {
+  alert("Empate, tentem novamente");
+} else {
+  if (sorteio === 1) {
+    if (n1 > n2) {
+      alert("Jogador 1 venceu!");
+    } else {
+      alert("Jogador 2 venceu!");
+    }
+  } else {
+    if (sorteio === 0) {
+      if (n1 < n2) {
+        alert("Jogador 1 venceu!");
+      } else {
+        ("Jogador 2 venceu!");
+      }
+    }
+  }
+}

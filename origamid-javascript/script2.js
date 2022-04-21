4bc594e416f9dc4607705c7cd59874490b1bb5af
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-21 20:00:00 -0300

    test: exercise with houver using JS

--- origamid-javascript/script2.js
+++ origamid-javascript/script2.js
@@ -1,6 +1,4 @@
-var numero1 = Number(prompt("Digite um número"));
-var numero2 = Number(prompt("Digite outro número"));
-
-var soma = numero1 + numero2;
-alert(`A soma é ${soma}`);
-
+function clicar() {
+    var a = window.document.getElementById('area');
+    a.innerText = 'Clicou!'
+}

564671b1d06ad9aca55f6e239e4e433e9c9c3194
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-21 15:03:31 -0300

    fix: new exercice

--- origamid-javascript/script2.js
+++ origamid-javascript/script2.js
@@ -1,5 +1,6 @@
-var numero1 Number(prompt("Digite um número"));
-var numero2 Number(prompt("Digite outro número"));
+var numero1 = Number(prompt("Digite um número"));
+var numero2 = Number(prompt("Digite outro número"));
 
-alert(numero1 + numero2);
+var soma = numero1 + numero2;
+alert(`A soma é ${soma}`);
 

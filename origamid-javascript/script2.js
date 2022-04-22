24a5d12a67c2dcd48ddfeb3894540617ef9bf061
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-22 09:28:53 -0300

    test: update exercise

--- origamid-javascript/script2.js
+++ origamid-javascript/script2.js
@@ -1,13 +1,19 @@
 var a = window.document.getElementById("area");
+a.addEventListener("click", clicar);
+a.addEventListener("mouseover", entrar);
+a.addEventListener("mouseout", sair);
 
 function clicar() {
   a.innerText = "Clicou!";
+  a.style.background = "yellow";
 }
 
 function entrar() {
   a.innerText = "Entrou!";
+  a.style.background = "red";
 }
 
 function sair() {
   a.innerText = "Saiu!";
+  a.style.background = "green";
 }

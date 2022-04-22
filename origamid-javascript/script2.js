ba94ed966b6f3f500bf2001bafaae3522429bfc3
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-22 09:07:13 -0300

    test: new exercise

--- origamid-javascript/script2.js
+++ origamid-javascript/script2.js
@@ -1,4 +1,13 @@
+var a = window.document.getElementById("area");
+
 function clicar() {
-    var a = window.document.getElementById('area');
-    a.innerText = 'Clicou!'
+  a.innerText = "Clicou!";
+}
+
+function entrar() {
+  a.innerText = "Entrou!";
+}
+
+function sair() {
+  a.innerText = "Saiu!";
 }

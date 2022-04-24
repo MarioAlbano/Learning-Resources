0ddc4779b604b4ebe1306461e187645bd030ef93
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-24 12:46:56 -0300

    test: fix new exercise

--- origamid-javascript/script.js
+++ origamid-javascript/script.js
@@ -1,16 +1,19 @@
 var a = window.document.getElementById("area");
+a.addEventListener("click", clicar);
+a.addEventListener("mouseover", entrar);
+a.addEventListener("mouseout", sair);
 
 function clicar() {
-    a.innerText = "Clicou!";
-    a.style.background = "red"
+  a.innerText = "Clicou!";
+  a.style.background = "red";
 }
 
 function entrar() {
-    a.innerText = "Entrou com o mouse"
-    a.style.background = "yellow"
+  a.innerText = "Entrou com o mouse";
+  a.style.background = "yellow";
 }
 
 function sair() {
-    a.innerText = "Saiu com o mouse"
-    a.style.background = "blue"
+  a.innerText = "Saiu com o mouse";
+  a.style.background = "blue";
 }

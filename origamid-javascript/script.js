99aed0bd3797d728bf834fbf72d5442d8cc360ba
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-24 22:13:50 -0300

    fixed files name

--- origamid-javascript/script.js
+++
@@ -1,9 +0,0 @@
-function somar() {
-  var tn1 = window.document.getElementById("textn1");
-    var tn2 = window.document.getElementById("textn2");
-    var res = window.document.getElementById("res");
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var s = n1 + n2;
-    res.innerHTML = `a soma entre ${n1} + ${n2} é igual a:<strong>${s}<strong>`
-}

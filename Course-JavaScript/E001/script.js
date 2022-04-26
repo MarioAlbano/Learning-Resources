d99df74751f72c345351f63c7c507560a22fde31
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-26 11:08:05 -0300

    test: testing new exercise

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,4 +1,10 @@
-let arredondar = Math.round(23.66);
-let arredondarCima = Math.ceil(23.11);
-console.log(arredondar);
-console.log(`minha idade é ${arredondar}`);
+let nome = "Mario";
+let idade = 26;
+
+if (nome != undefined && nome == "Draxler") {
+  console.log("primeiro passo feito");
+} else if(nome === "Mario" && idade == 26) {
+  console.log("Seu nome é mario");
+} else {
+  console.log("Você não é o Mario de 26 anos");
+}

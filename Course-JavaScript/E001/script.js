3977c766a0e98615b3e3930e70d6e3bd36cfa357
MarioAlbano <77970157+MarioAlbano@users.noreply.github.com>
2022-04-27 09:18:05 -0300

    fix: first exercise of the day

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,13 +1,16 @@
-let numero = 41;
+let recebe = 5;
 let div = 0;
 
-for (let i = 1; i <= numero; i++) {
-  if (numero % i == 0) {
+for (i = 1; i <= recebe; i++) {
+  console.log(i);
+  console.log("Divisão é" + div);
+  if (recebe % i == 0) {
     div++;
   }
 }
-if (div === 2) {
-  console.log(`${numero} é primo`);
+
+if (div == 2) {
+  console.log(`${recebe} É um número primo!`);
 } else {
-  console.log(`${numero} não é primo`);
+  console.log(`${recebe} Não é um número primo!`);
 }

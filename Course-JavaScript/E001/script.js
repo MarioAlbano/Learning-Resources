f2242e61f151a844bf4ad700691aa5da2df896fb
Mario Albano <marioalbanon@gmail.com>
2022-04-28 07:18:47 -0300

    first commit of the day

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,23 +1,6 @@
-// function multiplicarNumero(x, y, z) {
-//   return x * y * z;
-// }
-
-// console.log(multiplicarNumero(2, 4, 5));
-
-// const result = multiplicarNumero(5, 5, 5);
-
-// console.log(`Sue número é de ${result}`);
-
-function podeDirigir(x, y) {
-  if (x >= 18 && y == true) {
-    console.log(
-      `Como você tem ${x} anos e possui CNH (${y}), então pode dirigir`
-    );
-  } else {
-    console.log(`Você não tem ${x} idade suficiente ou não possui (${y}) cnh`);
-  }
+function baseElevada(base, elevado = 2) {
+  return Math.pow(base, elevado);
 }
 
-podeDirigir(25, true);
-podeDirigir(12, false);
-podeDirigir(77, 0);
+console.log(baseElevada(2));
+console.log(baseElevada(2, 3));

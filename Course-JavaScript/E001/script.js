d8de1d595a9d6d44d20905844d7e5aea0fbecacf
Mario Albano <marioalbanon@gmail.com>
2022-04-29 18:47:03 -0300

    test: exercice Object.assign

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,9 +1,24 @@
-function decrementaPares(x) {
-  for (x; x > 0; x--) {
-    if (x % 2 == 0) {
-      console.log(x + " é um número par");
-    }
-  }
-}
+let pessoa = {
+  nome: "Mario",
+  idade: 25,
+  localidade: "Brasil",
+  faculdade: "Ciências da computação",
+};
 
-decrementaPares(100000);
+let residencia = {
+  pais: "Brasil",
+  cidade: "São Paulo",
+  município: "Mairiporã",
+  Rua: "Rua logo ali, 246",
+};
+
+let residenciaCopia = { copia: "Somente um teste aqui" };
+
+console.log(pessoa);
+console.log("residencia sem método");
+console.log(residencia);
+
+Object.assign(residencia, residenciaCopia);
+//Copia os elementos do segundo para o fim do primeiro
+console.log("Agora com o método Object.assign");
+console.log(residencia);

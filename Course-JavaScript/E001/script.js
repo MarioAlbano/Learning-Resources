8d965129e21e784ef2de9fc080bf98f98e7c719c
Mario Albano <marioalbanon@gmail.com>
2022-04-30 11:22:21 -0300

    primeiro commit

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,13 +1,6 @@
-let nome = {
-  primeiroNome: "Mario",
-};
-
-nome2 = nome;
-
-let nome3 = {
-  primeiroNome: "Mario",
-};
-
-console.log(nome == nome2);
-console.log(nome == nome3);
-console.log(nome2 == nome3);
+let nomes = ["Mario", "Mario", "Jose", "Tyreon", "Teste", "Jackson"];
+i = 0;
+while (i <= nomes.length) {
+  console.log(nomes[i]);
+  i++;
+}

e0d864f79d58f6e1ccc74d5d108c1c588b3db295
Mario Albano <marioalbanon@gmail.com>
2022-05-04 11:47:52 -0300

    Destructuring em arrays

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,16 +1,8 @@
-let number1 = 1;
-let number2 = 2;
-let number3 = 3;
-let number4 = 4;
+let nomes = ["Mario", "Maria", "Jonson", "Marcia"];
 
-function printNumbers(...args) {
-  for (let i = 0; i < args.length; i++) {
-    console.log(args);
-    console.log(args[i]);
-  }
-}
+let [nomeA, nomeB, nomeC, nomeD] = nomes;
 
-printNumbers(number1, number2, number1, number4);
-console.log("pause");
-printNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-printNumbers("Este é um texte", "Este é um exemplo");
+console.log(nomeA);
+console.log(nomeB);
+console.log(nomeC);
+console.log(nomeD);

bed007f7338064e2397c80f315b30acfd9ddb643
Mario Albano <marioalbanon@gmail.com>
2022-05-04 18:10:50 -0300

    exercicio 2

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,3 +1,4 @@
-let arrayItems = [3, 1, 5, 6, 34];
+let arrayUm = [2, 4];
+let arrayDois = [5, 6, 7, 8];
 
-console.log(arrayItems[1 - 1], arrayItems[3 - 1], arrayItems[5 - 1]);
+console.log(arrayUm.length, arrayDois.length);

0ab9d2c5624613400598b6e9568fd4a5a29bd9cf
Mario Albano <marioalbanon@gmail.com>
2022-05-04 19:50:29 -0300

    Exercicio 6

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,5 +1,19 @@
-let nomes = ["Mario", "Joana", "Juan", "Marcia", "Heinsenberg"];
+let arrayUm = [1, 2, 3, 4, 5, 6, 8, 9, 0];
+let arrayDois = [1, 2, 3];
+let arrayTres = [1, 2, 3, 4, 5];
 
-if (nomes.includes("Mario")) {
-  console.log("Welcome again, Mario!");
+function quantidadeArray(x) {
+  if (x.length > 5) {
+    console.log("Muitos arrays");
+  }
+  else if (x.length == 5) {
+    console.log("Número certo de arrays!");
+  }
+  else {
+    console.log("Poucos arrays");
+  }
 }
+
+quantidadeArray(arrayUm);
+quantidadeArray(arrayDois);
+quantidadeArray(arrayTres);

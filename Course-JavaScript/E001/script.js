f015acf627e749669ad5d5d80821e697ee8bbb6b
Mario Albano <marioalbanon@gmail.com>
2022-05-04 11:32:59 -0300

    first commit

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,3 +1,16 @@
-let word = "palavras";
+let number1 = 1;
+let number2 = 2;
+let number3 = 3;
+let number4 = 4;
 
-console.log(word.repeat(400));
+function printNumbers(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args);
+    console.log(args[i]);
+  }
+}
+
+printNumbers(number1, number2, number1, number4);
+console.log("pause");
+printNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+printNumbers("Este é um texte", "Este é um exemplo");

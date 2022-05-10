baa6e5d7bb16700448a0ed5e0207e275b09157bc
Mario Albano <marioalbanon@gmail.com>
2022-05-10 15:44:42 -0300

    Métodos e propriedades em uma Class

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,14 +1,14 @@
 class Cachorro {
   constructor(raca, patas, cor) {
     this.raca = raca;
-    this.patas = patas;
     this.cor = cor;
   }
+  latir() {
+    console.log("Au au!");
+  }
 }
-
-Cachorro.prototype.raca = "SRD";
 Cachorro.prototype.patas = 4;
 
-let labrador = new Cachorro("Vira-Lata", 4, "Amarelo");
+let labrador = new Cachorro("Vira-Lata", "Amarelo");
 
-console.log(Cachorro.prototype.raca);
+labrador.latir();

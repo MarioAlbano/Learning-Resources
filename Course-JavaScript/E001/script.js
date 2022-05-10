aefdc13405b3427276f8efae9b32bd9355ab9675
Mario Albano <marioalbanon@gmail.com>
2022-05-10 10:28:01 -0300

    exercicio prototype

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,23 +1,6 @@
-const cachorro = {
-  raca: "vira-lata",
-  uivar: function () {
-    console.log("Auuuu");
-  },
-  latir: function () {
-    console.log("au au");
-  },
-  morder: function () {
-    console.log("Nhac!");
-  },
-  setRaca: function (raca) {
-    this.raca = raca;
-  },
-  getRaca: function () {
-    return "A raça é " + this.raca;
-  }
+const pessoa = {
+  maos: 2,
 }
 
-console.log(cachorro.raca);
-cachorro.setRaca("Pastore");
-console.log(cachorro.raca);
-console.log(cachorro.getRaca());
+console.log(Object.getPrototypeOf(pessoa));
+ 

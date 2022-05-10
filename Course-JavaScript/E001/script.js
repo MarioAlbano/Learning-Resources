50d99cbf3fffdc0c668f7d3880184eaa48659e7c
Mario Albano <marioalbanon@gmail.com>
2022-05-10 10:50:14 -0300

    Exercicio Prototype 2

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -3,4 +3,9 @@ const pessoa = {
 }
 
 console.log(Object.getPrototypeOf(pessoa));
- 
+
+const pessoaNova = Object.create(pessoa);
+
+console.log(pessoaNova.maos);
+console.log(pessoaNova.hasOwnProperty("maos"));
+console.log(Object.getPrototypeOf(pessoaNova) === pessoa);

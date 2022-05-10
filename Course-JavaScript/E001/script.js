301fc487132f609b94ab621fe038ef3b13603350
Mario Albano <marioalbanon@gmail.com>
2022-05-10 15:18:10 -0300

    Métodos no prototype

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -2,13 +2,19 @@ function Cachorro(raca, patas, cor) {
   this.raca = raca;
   this.patas = patas;
   this.cor = cor;
-  //criando um método dentro do objeto
-  this.uivar = function () {
-    console.log("Auuuuu!");
-  }
 }
+
+Cachorro.prototype.uivar = function () {
+  console.log("Auuuu");
+}
+
+Cachorro.prototype.latir =
+  function () {
+    console.log("Au Au!");
+  }
 
 let husky = new Cachorro("Husky", 4, "Preto");
 
 console.log(husky);
 console.log(husky.uivar());
+console.log(husky.latir());

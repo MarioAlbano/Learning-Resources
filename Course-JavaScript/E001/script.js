523b3ef11a4e72e59e08eb6c144a3b24bfb7ffd0
Mario Albano <marioalbanon@gmail.com>
2022-05-16 11:28:07 -0300

    resolucao exercicio 38

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,53 +1,31 @@
-class Carrinho {
-    constructor(itens, qtd, valorTotal) {
-        this.itens = itens;
-        this.qtd = qtd;
-        this.valorTotal = valorTotal;
+class enderecoCliente {
+    constructor(rua, bairro, cidade, estado) {
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
     }
 
-    //método de adicionar item ao carrinho
-    addItem(item) {
-
-        let contador = 0;
-        //criando um loop para acessar todos os itens do carrinho
-        for (let itemCarrinho in this.itens) {
-            //Está pesquisando entre os itens. Se o id for igual ao adicionado
-            if (this.itens[itemCarrinho].id == item.id) {
-                //se encontrar o mesmo ad, adiciona +1 ao item.qtd
-                this.itens[itemCarrinho].qtd += item.qtd;
-                contador = 1;
-            }
-        }
-
-        if (contador === 0) {
-            this, itens.push(item);
-        }
-        //atualizar o valor de quantidade dependendo do valor adicionado
-        this.qtd += item.qtd;
-        //atualizar os valores da compra dependendo do valor adicionado e a quantidade
-        this.valorTotal += item.preco * item.qtd;
+    attRua(att) {
+        this.rua = att;
+    }
+    attBairro(att) {
+        this.bairro = att;
+    }
+    attCidade(att) {
+        this.cidade = att;
+    }
+    attEstado(att) {
+        this.estado = att;
     }
 }
 
-//instanciando um objeto
-let carrinho = new Carrinho([
-    {
-        id: 01,
-        nome: "Camisa",
-        qtd: 1,
-        preco: 20
-    },
+let enderecoCliente01 = new enderecoCliente("Estrada st ines", "St ines", "sp", "sp");
+console.log(enderecoCliente01);
 
-    {
-        id: 02,
-        nome: "Calça",
-        qtd: 2,
-        preco: 50
-    }
-], 3, 120)
+enderecoCliente01.attRua("Rua ali por perto");
+enderecoCliente01.attBairro("Rua ali por perto");
+enderecoCliente01.attCidade("Rua ali por perto");
+enderecoCliente01.attEstado("Rua ali por perto");
 
-console.log(carrinho);
-
-carrinho.addItem({ id: 01, nome: "Camisa", qtd: 2, preco: 20 });
-
-console.log(carrinho);
+console.log(enderecoCliente01);

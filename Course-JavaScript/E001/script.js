9a6bf2bf0f771377f55f99815301839400b28edb
Mario Albano <marioalbanon@gmail.com>
2022-05-15 22:21:56 -0300

    Exercicio conta bancária

---
+++ Course-JavaScript/E001/script.js
@@ -0,0 +1,15 @@
+class Conta {
+    constructor(saldo) {
+        this.saldo = saldo;
+    }
+
+    deposito(valor) {
+        this.saldo += valor;
+    }
+}
+
+let conta = new Conta(1000);
+
+conta.deposito(1000);
+
+console.log(conta.saldo);

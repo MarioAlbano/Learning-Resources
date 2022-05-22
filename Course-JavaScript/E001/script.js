dfc240d9e4a53a9713afe0de61afbd183425a55f
Mario Albano <marioalbanon@gmail.com>
2022-05-22 19:22:03 -0300

    Exercicio 40 resolvido: Herança, métodos, prop

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -10,7 +10,29 @@ class contaBancaria {
     }
 
     saque(valor) {
+        this.saldoCorrente -= valor;
+    }
+
+    transferenciaPoupanca(valor) {
+        this.saldoCorrente -= valor;
+        this.saldoPoupanca += valor;
+    }
+
+    transferenciaCorrente(valor) {
+        this.saldoCorrente += valor;
         this.saldoPoupanca -= valor;
+    }
+
+    jurosAniversario(juros) {
+        let jurosConta = (this.saldoPoupanca * this.juros) / 100;
+        this.saldoPoupanca += jurosConta;
+    }
+}
+
+class contaBancaria02 extends contaBancaria {
+    constructor(saldoCorrente, saldoPoupanca, juros) {
+        super(saldoCorrente, saldoPoupanca, juros * 2)
+
     }
 }
 
@@ -22,3 +44,22 @@ console.log(conta);
 
 conta.deposito(10000);
 console.log(conta);
+
+conta.transferenciaCorrente(2222);
+console.log(conta);
+
+conta.transferenciaPoupanca(1111);
+console.log(conta);
+
+conta.jurosAniversario();
+console.log(conta);
+
+let conta02 = new contaBancaria02(10000, 50000, 1);
+console.log(conta02);
+
+conta02.saque(3000);
+console.log(conta02);
+
+conta02.jurosAniversario();
+console.log(conta02);
+

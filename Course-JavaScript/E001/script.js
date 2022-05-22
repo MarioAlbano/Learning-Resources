2adfb3b5aa9ecbd684cd46932e2145ddbe92efca
Mario Albano <marioalbanon@gmail.com>
2022-05-22 19:55:35 -0300

    strict JavaScript exemplo

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -3,4 +3,16 @@
 //testando usar uma variável sem declara-la
 let opa = "testando";
 
-delete Object.prototype;
+//Testando deletar uma função nativa do prototype
+// delete Object.prototype;
+
+function teste() {
+    //Usando strict dentro de uma função
+    "use strict"
+    let testando = "Teste";
+}
+
+teste();
+
+let objetoTeste = { p: 1, p: 2 };
+console.log(objetoTeste);

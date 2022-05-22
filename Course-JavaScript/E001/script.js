7c0fca0ae4ba9864796378a90c55cb6ab402b47d
Mario Albano <marioalbanon@gmail.com>
2022-05-22 18:52:38 -0300

    Ex 40 criando objeto

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,4 +1,17 @@
+class contaBancaria {
+    constructor(saldoCorrente, saldoPoupanca, juros) {
+        this.saldoCorrente = saldoCorrente;
+        this.saldoPoupanca = saldoPoupanca;
+        this.juros = juros;
+    }
 
+    deposito(valor) {
+        this.saldoCorrente
+    }
+}
+
+let conta = new contaBancaria(1000, 2000, 1);
+console.log(conta);
 
 
 

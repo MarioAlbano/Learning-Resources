c54747dadc0a077983510698088135731d1ae68f
Mario Albano <marioalbanon@gmail.com>
2022-05-22 18:58:20 -0300

    Criando método deposito e saque

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -6,56 +6,19 @@ class contaBancaria {
     }
 
     deposito(valor) {
-        this.saldoCorrente
+        this.saldoCorrente += valor;
+    }
+
+    saque(valor) {
+        this.saldoPoupanca -= valor;
     }
 }
 
 let conta = new contaBancaria(1000, 2000, 1);
 console.log(conta);
 
+conta.saque(300);
+console.log(conta);
 
-
-
-// class contaBancaria {
-//     constructor(saldoCorrente, saldoPoupanca, juros) {
-//         this.saldoCorrente = saldoCorrente;
-//         this.saldoPoupanca = saldoPoupanca;
-//         this.juros = juros;
-//     }
-//     depositoCorrente(valor) {
-//         this.saldoCorrente += valor;
-//     }
-//     depositoPoupanca(valor) {
-//         valor *= 1 - 0.05;
-//         this.saldoPoupanca += valor;
-//     }
-//     saqueCorrente(valor) {
-//         this.saldoCorrente -= valor;
-//     }
-//     saquePoupanca(valor) {
-//         this.saldoPoupanca -= valor;
-//     }
-
-//     transferenciaPoupancaCorrente(valor) {
-//         this.saldoPoupanca -= valor;
-//         this.saldoCorrente += valor;
-//     }
-// }
-
-// let contaJoao = new contaBancaria(1000, 1000, 5);
-// console.log(contaJoao);
-
-// contaJoao.depositoCorrente(200);
-// console.log(contaJoao);
-
-// contaJoao.depositoPoupanca(55);
-// console.log(contaJoao);
-
-// contaJoao.saqueCorrente(555);
-// console.log(contaJoao);
-
-// contaJoao.saquePoupanca(18);
-// console.log(contaJoao);
-
-// contaJoao.transferenciaPoupancaCorrente(200);
-// console.log(contaJoao);
+conta.deposito(10000);
+console.log(conta);

8a94201abaaefa5ab3b28114bec6d609ecc25109
Mario Albano <marioalbanon@gmail.com>
2022-05-22 21:52:28 -0300

    Exceptions

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,17 +1,11 @@
-function checagemNumero(num) {
-    //Transformando o objeto em number
-    let number = Number(num);
-
-    if (Number.isNaN(number)) {
-        console.log("Por favor, passe somente números para o programa");
-    } else {
-        return number;
+function saudacao(nome) {
+    if (typeof nome != "string") {
+        throw new Error("(Agora estou criando uma mensagem de erro aqui!!!) O parâmetro nome precisa ser uma string")
+    }
+    else {
+        console.log(`Olá ${nome}!`);
     }
 }
 
-checagemNumero(55);
-checagemNumero("Alok");
-
-let number = prompt("Digite um numero aqui");
-
-checagemNumero(number);
+saudacao("Rasputim");
+saudacao(12211221)

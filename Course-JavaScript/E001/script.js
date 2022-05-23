44b5396f0576de4fd9020e6e101a40daa5ee693d
Mario Albano <marioalbanon@gmail.com>
2022-05-22 21:59:42 -0300

    Try Catch em JavaScript

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,13 +1,5 @@
-function saudacao(nome) {
-    if (typeof nome != "string") {
-        throw new Error("(Agora estou criando uma mensagem de erro aqui!!!) O parâmetro nome precisa ser uma string")
-    }
-    else {
-        console.log(`Olá ${nome}!`);
-    }
+try {
+    let a = 2 + c;
+} catch (error) {
+    console.log(e);
 }
-
-saudacao("Rasputim");
-saudacao(12211221);
-
-console.log("Teste");

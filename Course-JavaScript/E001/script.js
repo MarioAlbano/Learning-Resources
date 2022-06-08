daf67ae39f05407bb06b1546f6522bb68a9c5f71
Mario Albano <marioalbanon@gmail.com>
2022-06-08 19:03:22 -0300

    Exercicio idade if else

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,11 +1,7 @@
-let variavelUm = "joao";
-let variavelDois = "Augusto";
+let idadeExercicio = 18;
 
-if (variavelUm && variavelDois == "joao") {
-    console.log("Entrou no if")
-} else if (variavelUm || variavelDois == "joao") {
-    console.log("Entrou no if 2");
-    console.log(!(3 > 2));
-} if (variavelUm == "joao") {
-    console.log("Entrou no if 3");
+if (idadeExercicio >= 18) {
+    console.log("Pode entrar");
+} else {
+    console.log("Você não tem a idade permitida");
 }

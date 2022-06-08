d2994bb7449ca76dd552cfaef8f78dccdf9fb09b
Mario Albano <marioalbanon@gmail.com>
2022-06-08 18:58:22 -0300

    Exercicio else if

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,2 +1,11 @@
-//usando o Strict mode
+let variavelUm = "joao";
+let variavelDois = "Augusto";
 
+if (variavelUm && variavelDois == "joao") {
+    console.log("Entrou no if")
+} else if (variavelUm || variavelDois == "joao") {
+    console.log("Entrou no if 2");
+    console.log(!(3 > 2));
+} if (variavelUm == "joao") {
+    console.log("Entrou no if 3");
+}

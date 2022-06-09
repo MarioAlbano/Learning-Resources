480aefa2ce9b56b6c9d7c885d272a6e5c6e0aff2
Mario Albano <marioalbanon@gmail.com>
2022-06-09 10:32:34 -0300

    While loop exercise

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,8 +1,5 @@
-let agePerson = 18;
-let driverLicense = true;
-
-if (agePerson >= 18 && driverLicense == true) {
-    console.log("Parabéns, você pode dirigir")
-} else {
-    console.log("Infelizmente você não pode dirigir");
+let countWhile = 1;
+while (countWhile <= 15) {
+    console.log(countWhile);
+    countWhile += 1;
 }

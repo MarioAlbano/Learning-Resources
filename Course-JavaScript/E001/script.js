b1b9bae399c6dba93f37926bb964ed6342825a9b
Mario Albano <marioalbanon@gmail.com>
2022-06-09 15:38:41 -0300

    reading each element of an array

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,3 +1,10 @@
-let arrayOne = [1, 2, 3, 4, 5];
+let arrayOne = [0, 1];
+let arrayTwo = [3, 4, 5, 6];
 
-console.log(arrayOne[1], arrayOne[3], arrayOne[4]);
+for (i = 0; i < arrayOne.length; i++) {
+    console.log(arrayOne[i]);
+}
+
+for (i = 0; i < arrayTwo.length; i++) {
+    console.log(arrayTwo[i]);
+}

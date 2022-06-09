24247a34946ec6814ecca1eaae0564f4350721e7
Mario Albano <marioalbanon@gmail.com>
2022-06-09 10:24:37 -0300

    Exercise velocity

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,3 +1,8 @@
-console.log(Math.pow(2, 2));
-console.log(Math.pow(3, 2));
-console.log(Math.pow(18, 2));
+let maxVelocity = 40;
+
+if (maxVelocity >= 80) {
+    console.log(`Sua velocidade é de ${maxVelocity}, portanto você recebeu uma multa`);
+}
+else {
+    console.log(`Parabéns, você está dentro da velocidade permitida`);
+}

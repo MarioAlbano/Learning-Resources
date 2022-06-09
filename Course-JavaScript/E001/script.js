5237a6a17ad2baa2f48cf2afd4d6a568d4d51131
Mario Albano <marioalbanon@gmail.com>
2022-06-09 10:51:46 -0300

    function example

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,14 +1,5 @@
-let primeNumberTest = 41;
-let divisionNumber = 0;
-
-for (let i = 0; i <= primeNumberTest; i++) {
-    if (primeNumberTest % i == 0) {
-        divisionNumber++;
-    }
+function helloWorld() {
+    console.log("Hello World!");
 }
 
-if (divisionNumber == 2) {
-    console.log(`Número ${primeNumberTest} é primo`);
-} else {
-    console.log(`Número ${primeNumberTest} não é primo`);
-}
+helloWorld();

199e36824991b1c3b67330afe5fa0841e55abeaf
Mario Albano <marioalbanon@gmail.com>
2022-06-09 11:37:50 -0300

    function limit of caracters

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,6 +1,10 @@
-function absoluteNumber(num) {
-    console.log(Math.abs(num));
+function textLength(text) {
+    if (text.length > 10) {
+        console.log(`Texto muito longo`);
+    } else {
+        console.log(`Texto dentro do limite`);
+    }
 }
 
-absoluteNumber(-55);
-absoluteNumber(101010);
+textLength("Textando esse texto aqui para ver se contém mais de dez caracteres!");
+textLength("Olá");

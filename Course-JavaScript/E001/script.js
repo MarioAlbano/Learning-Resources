42389db1dc238e4926421e411138d1df2744c29c
Mario Albano <marioalbanon@gmail.com>
2022-06-09 11:40:20 -0300

    function pow javascript

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,10 +1,5 @@
-function textLength(text) {
-    if (text.length > 10) {
-        console.log(`Texto muito longo`);
-    } else {
-        console.log(`Texto dentro do limite`);
-    }
+function powFunction(x, y) {
+    return Math.pow(x, y);
 }
 
-textLength("Textando esse texto aqui para ver se contém mais de dez caracteres!");
-textLength("Olá");
+console.log(powFunction(2, 6));

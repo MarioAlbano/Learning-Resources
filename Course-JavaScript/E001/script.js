cf35b1d528b7607ea1ce36cb3182a5d1f069645d
Mario Albano <marioalbanon@gmail.com>
2022-06-09 11:25:59 -0300

    max numbers in javascript

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,6 +1,8 @@
-function mathNumber(a, b) {
-    let resultNumber = a + b;
-    console.log(resultNumber);
+function maxRandom(a, b, c) {
+    a = Math.floor(Math.random() * a) + 1;
+    b = Math.floor(Math.random() * b) + 1;
+    c = Math.floor(Math.random() * c) + 1;
+    console.log(`The max number is ${Math.max(a, b, c)}`);
 }
 
-mathNumber(66, 6);
+console.log(maxRandom(10, 15, 20));

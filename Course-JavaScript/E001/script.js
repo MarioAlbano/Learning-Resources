3965065909a57e461045b5b4abc9f3c876700db1
Mario Albano <marioalbanon@gmail.com>
2022-06-09 10:49:39 -0300

    prime numbers in javascript exercise

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,5 +1,14 @@
-for (count = 0; count <= 50; count++) {
-    if (count % 2 == 0) {
-        console.log(count);
+let primeNumberTest = 41;
+let divisionNumber = 0;
+
+for (let i = 0; i <= primeNumberTest; i++) {
+    if (primeNumberTest % i == 0) {
+        divisionNumber++;
     }
+}
+
+if (divisionNumber == 2) {
+    console.log(`Número ${primeNumberTest} é primo`);
+} else {
+    console.log(`Número ${primeNumberTest} não é primo`);
 }

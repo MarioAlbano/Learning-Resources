4afc000053c937a8612fc52bad05e9e73767229e
Mario Albano <marioalbanon@gmail.com>
2022-06-11 11:48:09 -0300

    shopping cart exercise with class and method

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,20 +1,25 @@
-class bank {
-    constructor(balance) {
-        this.balance = balance
-        console.log(balance);
+class shoppingCart {
+    constructor(itens, amount, total) {
+        this.itens = itens;
+        this.amount = amount;
+        this.total = total;
     }
-    deposit(x) {
-        this.balance += x;
-        console.log(this.balance);
+    add(x, y) {
+        this.itens += x;
+        this.amount += y;
+        this.total += (x * y);
+        console.log(`Você tem ${this.itens} items, com a quantidade de ${this.amount}, totalizando em R$: ${this.total},00`)
     }
-    remove(x) {
-        this.balance -= x;
-        console.log(this.balance);
+    remove(x, y) {
+        this.itens -= x;
+        this.amount -= y;
+        this.total -= (x * y);
+        console.log(`Você tem ${this.itens} items, com a quantidade de ${this.amount}, totalizando em R$: ${this.total},00`)
     }
 }
 
-let accountOne = new bank(400);
+let cartOne = new shoppingCart(0, 0, 0);
 
-accountOne.balance;
-accountOne.deposit(100);
-accountOne.remove(300);
+cartOne.add(1, 3);
+cartOne.remove(1, 3);
+cartOne.add(5, 50);

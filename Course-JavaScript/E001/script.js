efdd0fedc73f749494cd3280809dd67226618dca
Mario Albano <marioalbanon@gmail.com>
2022-06-11 13:33:04 -0300

    Exercise bank account with deposit and draw

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,26 +1,47 @@
-class Car {
-    constructor(brand, color, gasAvailable) {
-        this.brand = brand;
-        this.color = color;
-        this.gasAvailable = gasAvailable;
-        console.log(`A marca é ${this.brand}, a cor é ${this.color} e tem ${this.gasAvailable} litros disponíveis no tanque.`);
+class Account {
+    constructor(balanceC, balanceP, taxP) {
+        this.balanceC = balanceC;
+        this.balanceP = balanceP;
+        this.taxP = taxP;
+        console.log(`Corrente: ${this.balanceC}. Poupança: ${this.balanceP}, Juros da Poupança: ${this.taxP}%.`)
     }
-    distance(x) {
-        this.gasAvailable -= (x / 11);
-        if (this.gasAvailable <= 0) {
-            console.log("Infelizmente acabou a sua gasolina!")
-        } else {
-            console.log(`Você tem ${this.gasAvailable} litros restante.`);
+    add(x, y) {
+        if (x == 1) {
+            this.balanceC += y;
+            console.log(`Sua conta correnta agora é ${this.balanceC}.`)
+        }
+        else if (x == 2) {
+            this.balanceP += y;
+            console.log(`Sua conta poupança agora é ${this.balanceC}.`)
+        }
+        else if (x == 3) {
+            this.balanceP -= (((this.taxP / 100) * y) + y);
+            this.balanceC += y;
+            console.log(`Você removeu da poupança ${y} reais restando em ${this.balanceP} com uma taxa de ${((this.taxP / 100) * y)} reais de juros. A conta corrente agora está em ${this.balanceC}`);
         }
     }
 
-    refuel(x) {
-        this.gasAvailable += x;
-        console.log(`Você reabasteceu o seu carro e agora você tem ${this.gasAvailable} litros disponíveis`);
+    remove(x, y) {
+        if (x == 1) {
+            this.balanceC -= y;
+            console.log(`Sua conta correnta agora é ${this.balanceC} pois você retirou ${y} reais.`)
+        }
+        else if (x == 2) {
+            this.balanceP -= y;
+            console.log(`Sua conta poupança agora é ${this.balanceC} pois você retirou ${y} reais`)
+        }
+    }
+}
+class accountEx extends Account {
+    constructor(balanceC, balanceP, taxP) {
+        super(balanceC, balanceP, taxP * 2);
     }
 }
 
-let saveiro = new Car("Volkswagen", "Preta", 40);
-saveiro.distance(0);
-saveiro.refuel(20);
-saveiro.distance(660);
+let accountOne = new Account(500, 1000, 5);
+accountOne.add(3, 100);
+accountOne.remove(1, 200);
+
+
+let accountTwo = new accountEx(10000, 20000, 10);
+accountTwo.add(3, 1000);

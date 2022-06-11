fce8637bbc1225bfb20e54927934b416a70d3540
Mario Albano <marioalbanon@gmail.com>
2022-06-11 12:25:29 -0300

    exercise car with methods

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,30 +1,26 @@
-class address {
-    constructor(road, district, city, state) {
-        this.road = road;
-        this.district = district;
-        this.city = city;
-        this.state = state;
-        console.log(`Sua rua é ${this.road}, o distrito é ${district}, a cidade é ${this.city} e o estado é ${this.state}`);
-        console.log("Digite para alterar: 1 - Rua; 2 - Distrito; 3 - Cidade; 4 - Estado")
+class Car {
+    constructor(brand, color, gasAvailable) {
+        this.brand = brand;
+        this.color = color;
+        this.gasAvailable = gasAvailable;
+        console.log(`A marca é ${this.brand}, a cor é ${this.color} e tem ${this.gasAvailable} litros disponíveis no tanque.`);
     }
-    change(x, y) {
-        if (x == 1) {
-            this.road = y;
+    distance(x) {
+        this.gasAvailable -= (x / 11);
+        if (this.gasAvailable <= 0) {
+            console.log("Infelizmente acabou a sua gasolina!")
+        } else {
+            console.log(`Você tem ${this.gasAvailable} litros restante.`);
         }
-        if (x == 2) {
-            this.district = y;
-        }
-        if (x == 3) {
-            this.dity = y;
-        }
-        if (x == 4) {
-            this.state = y;
-        }
-        console.log(`Sua rua é ${this.road}, o distrito é ${this.district}, a cidade é ${this.city} e o estado é ${this.state}`);
-        console.log("Digite para alterar: 1 - Rua; 2 - Distrito; 3 - Cidade; 4 - Estado")
+    }
+
+    refuel(x) {
+        this.gasAvailable += x;
+        console.log(`Você reabasteceu o seu carro e agora você tem ${this.gasAvailable} litros disponíveis`);
     }
 }
 
-let addresOne = new address("Estrada Sanda Ines", "MP", "SP", "São Paulo");
-
-addresOne.change(1, "Est Mun prefeito doutor sarkis tellian");
+let saveiro = new Car("Volkswagen", "Preta", 40);
+saveiro.distance(0);
+saveiro.refuel(20);
+saveiro.distance(660);

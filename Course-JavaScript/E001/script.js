84cc3ec10ca9e06bd5fa6c3356ff17542534386a
Mario Albano <marioalbanon@gmail.com>
2022-06-12 11:19:40 -0300

    Regex 02 Javascript

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -2,3 +2,10 @@ let regexOne = new RegExp("amigos");
 
 console.log(regexOne.test("Olá amigos, bem vindo a mais um video de: "));
 console.log(regexOne.test("Hoje na segunda guerra mundial!"));
+
+let regenxTwo = /guerra/;
+
+console.log(regenxTwo.test("Hoje na segunda"));
+console.log(regenxTwo.test("guerra mundial"));
+console.log("Agora o segundo teste");
+console.log(/teste/.test("teste"));

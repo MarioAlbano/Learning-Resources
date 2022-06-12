58c07b9171c6886ffa3313b486d9b9871384f6ab
Mario Albano <marioalbanon@gmail.com>
2022-06-12 19:51:42 -0300

    validadando email com regex javascript

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,6 +1,4 @@
-let validateSite = /www.\w+.(com|com.br)/;
+let validateEmail = /\w+@+\w+.(com|com.br)/
 
-console.log(validateSite.test("www.google.com.br"));
-console.log(validateSite.test("www.google.org"));
-console.log(validateSite.test("www.teste.com"));
-console.log(validateSite.test("www.w.com"));
+console.log(validateEmail.test("marioalbanon@gmail.com"));
+console.log(validateEmail.test("ignacio@hotmail.org"));

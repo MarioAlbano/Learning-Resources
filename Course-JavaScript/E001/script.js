6dccf40181d243eaf03c49931a1abd820075d134
Mario Albano <marioalbanon@gmail.com>
2022-06-13 08:56:20 -0300

    regex ip test

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,5 +1,6 @@
-let brandTest = /[Marca:] [|Nike|Adidas|Puma|Asics]/;
+let ipTest = /\d{1-3}\.\d{1-3}\.\d{1-3}\.\d{1-3}/
 
-console.log(brandTest.test("Marca: Nike"));
-console.log(brandTest.test("Marca: Mcdonalds"));
-console.log(brandTest.test("Marca: Puma"));
+console.log(ipTest.test("127777777777777.0.0.1"));
+console.log(ipTest.test("127.0.000.1"));
+console.log(ipTest.test("127.00.0.1"));
+console.log(ipTest.test("127.0.0.11"));

c91fac2d61358809ca3eff8608415b84b6d452c0
Mario Albano <marioalbanon@gmail.com>
2022-06-13 08:33:51 -0300

    Regex Uppercase

--- Course-JavaScript/E001/script.js
+++ Course-JavaScript/E001/script.js
@@ -1,15 +1,8 @@
-let validadeYear = /(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19\d{2}|20[0-2][0[1-9]|1[0-9]|2[0-2])/
+let onlyCaps = /^[^a-z]*$/;
 
-console.log(validadeYear.test("28/06/1995"));
-console.log(validadeYear.test("abc/06/1995"));
-console.log(validadeYear.test("31/07/1945"));
-console.log(validadeYear.test("12/12/1999"));
-console.log(validadeYear.test("31/01/2023"));
-console.log(validadeYear.test("12/06/2025"));
-console.log(validadeYear.test("31/01/2077"));
 
-2000
-2010
-2019
-2020
-2022
+console.log(onlyCaps.test("TESTE"));
+console.log(onlyCaps.test("teste"));
+console.log(onlyCaps.test("123"));
+console.log(onlyCaps.test("Testando"));
+console.log(onlyCaps.test("TEEESSSTTTEEEEE"));

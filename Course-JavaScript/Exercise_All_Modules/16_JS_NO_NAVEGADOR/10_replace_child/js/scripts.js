fff3fb7da79bd992ae4c5fbd1a56816335b565f9
Mario Albano <marioalbanon@gmail.com>
2022-06-22 14:34:39 -0300

    Posições dos elementos em javascript

--- Course-JavaScript/Exercise_All_Modules/16_JS_NO_NAVEGADOR/10_replace_child/js/scripts.js
+++ Course-JavaScript/Exercise_All_Modules/16_JS_NO_NAVEGADOR/10_replace_child/js/scripts.js
@@ -1,4 +1,3 @@
 let elemento = document.querySelector("#titulo-principal");
 
-console.log("Largura " + elemento.offsetWidth); //Considera as bordas;
-console.log("Altura " + elemento.offsetHeight); //
+console.log(elemento.getBoundingClientRect());

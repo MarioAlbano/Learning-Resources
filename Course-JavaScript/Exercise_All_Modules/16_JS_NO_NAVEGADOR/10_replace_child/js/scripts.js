6696fd516b469a16387196b035a19533493af7b4
Mario Albano <marioalbanon@gmail.com>
2022-06-22 11:09:18 -0300

    Utilizando o InsertBefore

--- Course-JavaScript/Exercise_All_Modules/16_JS_NO_NAVEGADOR/10_replace_child/js/scripts.js
+++ Course-JavaScript/Exercise_All_Modules/16_JS_NO_NAVEGADOR/10_replace_child/js/scripts.js
@@ -1,9 +1,18 @@
+// let novoElemento = document.createElement("p");
+// let texto = document.createTextNode("Algum texto");
+
+// novoElemento.appendChild(texto);
+
+// let heading = document.querySelector('#titulo-principal');
+// let paiHeading = heading.parentNode;
+
+// paiHeading.replaceChild(novoElemento, heading);
+
 let novoElemento = document.createElement("p");
-let texto = document.createTextNode("Algum texto");
+let elementoAlvo = document.querySelector("#titulo-principal");
+let texto = document.createTextNode("Um texto inserido através do 'document.createTextNode'");
 
 novoElemento.appendChild(texto);
 
-let heading = document.querySelector('#titulo-principal');
-let paiHeading = heading.parentNode;
-
-paiHeading.replaceChild(novoElemento, heading);
+let elementoPai = document.querySelector("#container-principal");
+elementoPai.insertBefore(novoElemento, elementoAlvo);

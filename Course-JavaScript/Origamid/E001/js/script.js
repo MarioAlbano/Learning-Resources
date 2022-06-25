33eb3a536078c9aac8c551db179e7dfcb3d989d2
Mario Albano <marioalbanon@gmail.com>
2022-06-25 10:29:15 -0300

    Adicionado slides de javascript e add exercicio

--- Course-JavaScript/Origamid/E001/js/script.js
+++ Course-JavaScript/Origamid/E001/js/script.js
@@ -1,6 +1,15 @@
-function imc(peso, altura) {
-    let imc = peso / (altura * altura);
-    return imc;
+function amostraUm() {
+    let nome = "Mario",
+        idade = 26,
+        profissao = "Developer";
+    // return `${nome}, ${idade}, ${profissao}`;
+    function amostraDois() {
+        let nome = "Marcia",
+            idade = 49,
+            profissao = "Advogada";
+        return `${nome}, ${idade}, ${profissao}`;
+    }
+    return amostraDois()
 }
 
-console.log(imc(88, 1.75));
+console.log(amostraUm());

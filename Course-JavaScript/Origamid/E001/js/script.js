92345299dda7e6f4263a1c83b090564facd78a0a
Mario Albano <marioalbanon@gmail.com>
2022-06-26 11:26:18 -0300

    Instalado react e iniciado um projeto

--- Course-JavaScript/Origamid/E001/js/script.js
+++ Course-JavaScript/Origamid/E001/js/script.js
@@ -1,15 +1,10 @@
-function amostraUm() {
-    let nome = "Mario",
-        idade = 26,
-        profissao = "Developer";
-    // return `${nome}, ${idade}, ${profissao}`;
-    function amostraDois() {
-        let nome = "Marcia",
-            idade = 49,
-            profissao = "Advogada";
-        return `${nome}, ${idade}, ${profissao}`;
+//Parte 1
+function verificarValor(x) {
+    if (typeof x === "boolean") {
+        return `O valor ${x} é booleano!`;
+    } else {
+        return `Não é booleano`;
     }
-    return amostraDois()
 }
 
-console.log(amostraUm());
+//Parte 2

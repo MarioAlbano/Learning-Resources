0067416e6c7142a261a7aeca90f4af80d888d8df
Mario Albano <marioalbanon@gmail.com>
2022-06-26 12:12:46 -0300

    Adicionado emmet para react

--- Course-React/E001/hello-world/src/App.js
+++ Course-React/E001/hello-world/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Testando o emmet no react!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus consequuntur quibusdam, repudiandae labore aliquid magni doloremque tempore repellendus, quo nesciunt aliquam, sunt porro libero adipisci. Fuga, sint adipisci. Ipsam, dolore?</p>
     </div>
   );
 }

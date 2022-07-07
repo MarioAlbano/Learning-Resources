92e0895955cb0e4da2c7b03c49cd8eed67044624
Mario Albano <marioalbanon@gmail.com>
2022-07-07 11:55:54 -0300

    Fix: arrumado arquivos e criado novas pastas de estudo

--- Rocketseat-Discover/E00/scripts.js
+++ Rocketseat-Discover/E00/scripts.js
@@ -1,36 +1,8 @@
-// let weight;
+let umDoisDois = "122";
+let doisUmUm = 211;
 
-// console.log(typeof weight);
+console.log(umDoisDois);
+console.log(doisUmUm);
 
-// const name = "Mario";
-// let age = 27;
-// let starts = 4.75;
-// let isSubscribed = True;
-
-
-let student = {
-    name: "Mario",
-    age: 27,
-    starts: 4.75,
-    weight: 88.5,
-    isSubscribed: true,
-};
-console.log(student);
-
-console.log(`${student.name} de idade ${student.age} e ${student.weight} kg.`);
-
-let students = [student];
-
-console.log(students[0]);
-
-const studentTwo = {
-    name: "John",
-    age: 17,
-    starts: 2.75,
-    weight: 68.5,
-    isSubscribed: false,
-}
-
-students[1] = studentTwo;
-
-console.log(students[1]);
+console.log(Number(umDoisDois));
+console.log(String(doisUmUm)); 

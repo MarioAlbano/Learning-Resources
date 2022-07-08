40da6f306fe23dae5d97ec803cf722770252c183
Mario Albano <marioalbanon@gmail.com>
2022-07-08 10:20:44 -0300

    Docs: adicionada informações react

--- Course-React-Udemy/E001/1_INTRO/hello-world/src/components/FirstComponent.js
+++ Course-React-Udemy/E001/1_INTRO/hello-world/src/components/FirstComponent.js
@@ -1,9 +1,13 @@
 //Arquivo de estilo
 
+import MyComponent from "./MyComponent";
+import TemplateExpressions from "./TemplateExpressions";
+
 const FirstComponent = () => {
     //Essa função cria meu primeiro componente
     return (
         < div >
+            <MyComponent />
             <h1>Meu primeiro componente</h1>
             {/*ESte é um comentário JSX*/}
         </div >

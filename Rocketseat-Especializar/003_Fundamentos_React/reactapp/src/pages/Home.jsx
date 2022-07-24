4de092cca22d0d3f62e847e8c633f0244bb4bf76
Mario Albano <marioalbanon@gmail.com>
2022-07-24 09:38:17 -0300

    Feat: adicionado arquivos React

---
+++ Rocketseat-Especializar/003_Fundamentos_React/reactapp/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+
+  return (
+    <>
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </>
+  )
+}
+
+export default Home

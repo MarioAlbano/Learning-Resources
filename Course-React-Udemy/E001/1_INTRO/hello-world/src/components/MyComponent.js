48edc8dcd3deac8a10b9777e39a5221de4c40bf8
Mario Albano <marioalbanon@gmail.com>
2022-08-02 14:59:58 -0300

    Fix: resetado react

--- Course-React-Udemy/E001/1_INTRO/hello-world/src/components/MyComponent.js
+++
@@ -1,9 +0,0 @@
-const MyComponent = () => {
-    return (
-        <div>
-            <h2>Eu estou sendo reaproveitado em vários lugares!</h2>
-        </div>
-    )
-}
-
-export default MyComponent;

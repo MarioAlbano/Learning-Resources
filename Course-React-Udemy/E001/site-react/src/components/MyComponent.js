c730f3b66645a4988fc3249cac5ec8607d8d91e4
Mario Albano <marioalbanon@gmail.com>
2022-08-03 00:14:44 -0300

    Feat: exemplificado hierarquia componentes

---
+++ Course-React-Udemy/E001/site-react/src/components/MyComponent.js
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+    return (
+        <div>
+            <h2>Sendo Reaproveitando com este componente</h2>
+        </div>
+    )
+}
+
+export default MyComponent;
--- Course-React-Udemy/E001/site-react/src/components/primeiroComponente.js
+++ Course-React-Udemy/E001/site-react/src/components/primeiroComponente.js
@@ -1,9 +1,11 @@
+import MyComponent from './MyComponent';
 //Para criar o componente, temos que criar a função e o retorno
 const PrimeiroComponente = () => {
     return (
         <div>
             {/*Comentário*/}
             <h3>primeiroComponente</h3>
+            <MyComponent />
         </div>
     );
 };

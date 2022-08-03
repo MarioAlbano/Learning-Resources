f3ad69b7f48dac2126e902377174efbdee260880
Mario Albano <marioalbanon@gmail.com>
2022-08-03 00:07:03 -0300

    Feat: adicionado novos arquivos react

--- Course-React-Udemy/E001/site-react/src/App.js
+++ Course-React-Udemy/E001/site-react/src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
+//Arquivos Componentes
+import PrimeiroComponente from './components/primeiroComponente';
+import TemplateExpressions from './components/TemplateExpressions';
+//Arquivos CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div></div>
-      <h1>Hello World! Teste de fast reloaded</h1>
+      <section className="section-react">
+        <h1>Hello World! Teste de fast reloaded</h1>
+        <PrimeiroComponente />
+        <h2>Testando o emmet no react</h2>
+        <TemplateExpressions />
+      </section>
     </div>
   );
 }
 
 export default App;
-
-
---
+++ Course-React-Udemy/E001/site-react/src/components/primeiroComponente.js
@@ -0,0 +1,12 @@
+//Para criar o componente, temos que criar a função e o retorno
+const PrimeiroComponente = () => {
+    return (
+        <div>
+            {/*Comentário*/}
+            <h3>primeiroComponente</h3>
+        </div>
+    );
+};
+
+//exportando para poder reutilizar
+export default PrimeiroComponente;

6734c5b95a4926a9feccf253b9dac4cb016c7463
Mario Albano <marioalbanon@gmail.com>
2022-08-03 22:20:09 -0300

    Feat: imagem em asset

--- Course-React-Udemy/E002/avancando-react/src/App.js
+++ Course-React-Udemy/E002/avancando-react/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Img1 from "./assets/img1.jpg"
 
 function App() {
   return (
     <div className="div-grid">
       <h1>Aprendendo React</h1>
       <img src="/img1.jpg" alt="Uma paisagem ao nascer do sol" />
-    </div>
+      <div>
+        <img src={Img1} alt="" />
+      </div>
+
+    </div >
+
   );
 }
 

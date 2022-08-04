26a39d1a989ec61e8ddd57c73c2aae85dd7349f3
Mario Albano <marioalbanon@gmail.com>
2022-08-04 16:58:59 -0300

    Feat: adicionando prev state react

--- Course-React-Udemy/E002/avancando-react/src/componets/ListRender.js
+++ Course-React-Udemy/E002/avancando-react/src/componets/ListRender.js
@@ -1,7 +1,21 @@
 import { useState } from 'react'
 
 const ListRender = () => {
-    const [nomesFuncionarios] = useState(["Mario", "Hamilton", "Batshuayi"])
+    const [nomesFuncionarios] = useState(["Mario", "Hamilton", "Batshuayi"]);
+
+    const [users, setUsers] = useState([
+        { nome: "Mario", idade: 27, id: 1 },
+        { nome: "Hamilton", idade: 43, id: 2 },
+        { nome: "Batshuayi", idade: 28, id: 3 }
+    ]);
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id)
+        });
+    };
 
     return (
         <div>
@@ -10,6 +24,7 @@ const ListRender = () => {
                     (item, i) => <li key={i}>{item}</li>
                 )}
             </ul>
+            <button onClick={deleteRandom}>Deletar usuário</button>
         </div>
     )
 }

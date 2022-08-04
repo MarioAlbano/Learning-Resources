60f960b0eefdcc2013ed41a1af0f8bda842ac2bb
Mario Albano <marioalbanon@gmail.com>
2022-08-03 22:13:34 -0300

    Feat: adicionado imagem de forma src

--- Course-React-Udemy/E002/avancando-react/src/App.js
+++ Course-React-Udemy/E002/avancando-react/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="div-grid">
+      <h1>Aprendendo React</h1>
+      <img src="/img1.jpg" alt="Uma paisagem ao nascer do sol" />
     </div>
   );
 }

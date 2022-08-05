c5a01a87df7a8d3ab4e057cfc62579470acf187a
Mario Albano <marioalbanon@gmail.com>
2022-08-05 10:09:00 -0300

    Feat: exercicio entrega de dados 2

--- one-bit-fullstack/E001-Javascript-Logica/E001/script.js
+++ one-bit-fullstack/E001-Javascript-Logica/E001/script.js
@@ -1,11 +1,14 @@
-alert("Bem-vindo! A seguir pediremos que informe alguns dados");
-
-let perguntaNome = prompt("Qual o seu nome?");
-let perguntaIdade = prompt("Qual a sua idade?");
-let perguntaConfirma = confirm(`Sua nome é ${perguntaNome} e possui ${perguntaIdade} anos?`)
-
-alert(`${perguntaNome} \n ${perguntaIdade} \n ${perguntaConfirma} `);
-
 //Comentário de uma linha
 /*Comentário de
 Múltiplas linhas*/
+
+const nomeMaior = prompt("Pessoa A: Qual o seu nome?");
+const idadeMaior = prompt("Pessoa A: Qual a sua idade");
+
+const nomeMenor = prompt("Pessoa B: Qual o seu nome?");
+const idadeMenor = prompt("Pessoa B: Qual a sua idade?");
+
+let soma = (idadeMaior - idadeMenor);
+
+alert(`${nomeMaior} ${idadeMaior} \n\n ${nomeMenor} ${idadeMenor}`);
+alert(`A diferença entre a idade é de ${soma} anos`);

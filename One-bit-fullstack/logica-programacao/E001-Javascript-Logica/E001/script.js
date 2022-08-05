7b6bfe5822192c0af5298d2c240175e652639335
Mario Albano <marioalbanon@gmail.com>
2022-08-05 19:48:59 -0300

    Feat: switch case

---
+++ One-bit-fullstack/logica-programacao/E001-Javascript-Logica/E001/script.js
@@ -0,0 +1,12 @@
+let velocity = 100;
+
+switch (velocity * 2) {
+    case 160:
+        console.log("Sua velocidade é 160");
+        break
+    case 200:
+        console.log("Sua velocidade é 200");
+        break
+    default:
+        console.log("Velocidade não encotrada");
+}

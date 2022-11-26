af5d685b58ba73a0c5774126dcdc7e914a171015
Mario Albano <marioalbanon@gmail.com>
2022-11-26 20:47:59 -0300

    Feat: added basic examples javascript

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/01-primitivos/script.js
@@ -0,0 +1,13 @@
+console.log(20 + "22");
+console.log(20 + 22);
+
+const b = 55;
+
+console.log(b + 10);
+
+console.log("javascript".length + 2 * 8);
+console.error("Tente novamente");
+
+let name = window.prompt();
+console.log(name);
+window.confirm("Teste?"); 

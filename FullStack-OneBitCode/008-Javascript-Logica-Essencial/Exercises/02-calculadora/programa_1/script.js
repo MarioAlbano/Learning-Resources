c35abaf3f35992dfb91fcaef76dc9421e4a5fd36
Mario Albano <marioalbanon@gmail.com>
2022-11-26 21:03:51 -0300

    Feat: added exercise 02

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/02-calculadora/programa_1/script.js
@@ -0,0 +1,7 @@
+let userName = prompt("Qual seu primeiro nome?");
+let lastName = prompt("Qual seu sobrenome?");
+let field = prompt("Qual sua área?");
+let dateBirth = prompt("Em que ano você nasceu?");
+let years = (2022 - dateBirth);
+
+alert(`Seu nome é ${userName}, Sobrenome: ${lastName}, atuando na área de ${field}, com ${years} anos.`);

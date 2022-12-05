b6f5c8de54e5e1320bbc0f5ec65de317ad3541e3
Mario Albano <marioalbanon@gmail.com>
2022-12-05 19:20:25 -0300

    Feat: array test in javascript

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/09-Arrays/script.js
@@ -0,0 +1,9 @@
+let userSet = ["Mario", 27, "Solteiro", "Brasileiro", "Segunda nacionalidade: Japonês"];
+
+console.log(userSet[0]);
+console.log(userSet[1]);
+console.log(userSet[2]);
+console.log(userSet[3]);
+console.log(userSet[4]);
+//Expected undefined
+console.log(userSet[5]);

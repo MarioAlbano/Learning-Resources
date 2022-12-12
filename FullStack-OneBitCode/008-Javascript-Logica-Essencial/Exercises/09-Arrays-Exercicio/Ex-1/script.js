4bff9bc123fd471c6253e20ee727a5e950b2a199
Mario Albano <marioalbanon@gmail.com>
2022-12-12 20:39:59 -0300

    Feat: new exercise hospital

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/09-Arrays-Exercicio/Ex-1/script.js
@@ -0,0 +1,26 @@
+//Creating array
+let hospitalQueue = ["Marcos", "Matheus", "Juan", "Carlos", "Giovanni", "D'Angelo"];
+
+//Testing array
+console.log(hospitalQueue);
+
+//Asking option
+userOption = prompt("Bem vindo ao hospital Cantareira!" + "\nQual opção deseja?" + "\n" + "\n1- Inserir novo paciente" + "\n2- Consultar paciente" + "\n3- Sair");
+
+do {
+  if (userOption = 1) {
+    insertName = prompt("Qual o nome do novo paciente?");
+    hospitalQueue.push(insertName);
+  }
+  else if (userOption = 2) {
+    hospitalQueue.includes()
+  }
+
+  else if (userOption < 1 || userOption > 0) {
+    alert("Opção não identificada. Por favor tente novamente!");
+  }
+
+
+} while (userOption != 3);
+
+alert("Finalizando sistema... Até breve!");

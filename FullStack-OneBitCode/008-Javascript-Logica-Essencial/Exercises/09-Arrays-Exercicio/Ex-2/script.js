96dfe1c0c217b6170e94de2667461dedab027fb6
Mario Albano <marioalbanon@gmail.com>
2022-12-21 16:17:22 -0300

    Feat: array cards exercise

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/09-Arrays-Exercicio/Ex-2/script.js
@@ -0,0 +1,30 @@
+//Initializing Cards Array
+let cardList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+let userOption;
+
+//System starting
+alert("Boas vinda ao Royal Cards!");
+
+//Initializing Loop
+do {
+  let showCards = "";
+  for (i = 0; i < cardList.length; i++) {
+    showCards += `"${cardList[i]}" `;
+  }
+  alert(`Cartas do baralho: ${showCards}.`);
+
+  userOption = prompt("Digite uma opção:" + "\n" + "1- Adicionar uma carta " + "\n" + "2- Puxar uma carta" + "\n" + "3- Sair");
+
+  if (userOption == "1") {
+    const newCard = prompt("Qual o nome da carta?");
+    cardList.push(newCard);
+  } else if (userOption == "2") {
+    const removedCard = cardList.pop();
+    alert(`A carta retirada foi a ${removedCard}.`);
+  }
+  else if (userOption == "4") {
+    alert("Desculpe, opção inválida. Tente novamente por favor.");
+  }
+} while (userOption != "3")
+
+alert("Saindo do sistema. Obrigado pela visita!");

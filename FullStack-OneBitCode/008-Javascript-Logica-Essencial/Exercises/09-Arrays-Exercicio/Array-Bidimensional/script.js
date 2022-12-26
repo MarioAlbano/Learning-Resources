b5dd04b0d099497884cda97bfa6566bd65b7c567
Mario Albano <marioalbanon@gmail.com>
2022-12-26 20:44:57 -0300

    Feat: Added two-dimensional array exercise

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/09-Arrays-Exercicio/Array-Bidimensional/script.js
@@ -0,0 +1,13 @@
+//Testing two-dimensional arrays
+const firstGroup = [[["A1"], ["A2"]],
+[["B1"], ["B2"]], [["C1"], ["C2"]], [["D1"], ["D2"]]];
+
+//Showing table on console
+console.table(firstGroup);
+
+//accessing arrays
+console.log(firstGroup[0]);
+console.log(firstGroup[1]);
+console.log(firstGroup[2]);
+console.log(firstGroup[0][0]);
+console.log(firstGroup[1][1]);

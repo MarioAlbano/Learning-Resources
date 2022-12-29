d32551bdd3b02df13b6b7113811579b2b2b9ea33
Mario Albano <marioalbanon@gmail.com>
2022-12-28 21:30:19 -0300

    Docs: added var notes

---
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/11-Funcoes/funcoes-exemplos/script.js
@@ -0,0 +1,34 @@
+//Creating functions for testing
+function firstFunction() {
+  console.log("Olá mundo!");
+}
+
+firstFunction();
+
+function doulbleNumber(x = 0) {
+  console.log(`Seu número ${x} vezes dois terá o resultado de ${x * 2}`);
+}
+
+doulbleNumber(3);
+doulbleNumber();
+
+//Testing object as parameter
+const objectParameter = {
+  nome: "Silva",
+  telefone: "12345",
+  email: "silva@outlook.com",
+  senha: "54321"
+}
+
+function testingParameter(x) {
+  console.log(x.nome);
+}
+
+testingParameter(objectParameter);
+
+//Return functions
+function returnFunction(x) {
+  return (x * 3);
+}
+
+console.log(returnFunction(3));

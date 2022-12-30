4e2c0b6dece8cf187113c8dc3529bd8abd7bbca8
Mario Albano <marioalbanon@gmail.com>
2022-12-29 23:25:43 -0300

    Feat: examples recursive functions

--- FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/12-Metodos/script.js
+++ FullStack-OneBitCode/008-Javascript-Logica-Essencial/Exercises/12-Metodos/script.js
@@ -1,5 +1,20 @@
 const personObject = {
   personName: "Mario",
   personAge: 27,
-  personObject.personName = "Romário"
+  // personObject.personName = "Romário" //Expected error here
 }
+
+function recursiveFunction(num) {
+  console.log(`Número ${num}`);
+  if (num == 0) {
+    return 1;
+  }
+  else if (num == 1) {
+    return 1;
+  }
+  else {
+    return num * recursiveFunction(num - 1);
+  }
+}
+
+console.log(recursiveFunction(5));

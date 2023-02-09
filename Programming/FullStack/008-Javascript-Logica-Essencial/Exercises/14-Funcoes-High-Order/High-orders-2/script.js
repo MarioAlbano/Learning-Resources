d8f8a5a968a2dc2351acdb0c1adb8d20e7d6b763
Mario Albano <marioalbanon@gmail.com>
2023-02-08 21:14:58 -0300

    feat: added high orders function exercise

--- Programming/FullStack/008-Javascript-Logica-Essencial/Exercises/14-Funcoes-High-Order/High-orders-2/script.js
+++ Programming/FullStack/008-Javascript-Logica-Essencial/Exercises/14-Funcoes-High-Order/High-orders-2/script.js
@@ -1,25 +1,13 @@
-//Select each element with For
-const characters = [
-  { level: 42, name: "Thrall", race: "Orc", class: "Xamã" },
-  { level: 28, name: "Garrosh", race: "Orc", class: "Guerreiro" },
-  { level: 35, name: "Varok", race: "Orc", class: "Guerreiro" },
-  { level: 35, name: "Uther", race: "Humano", class: "Paladino" },
-  { level: 26, name: "Jaina", race: "Humano", class: "Maga" },
-  { level: 39, name: "Tyrande", race: "Elfo Noturno", class: "Sacerdotisa" },
-  { nivel: 29, name: "Muradin", raca: "Anão", class: "Guerreiro" },
-];
-
-let selectedCharacters = [];
-
-for (i = 0; i < characters.length; i++) {
-  selectedCharacters.push(characters[i].name);
+//Creating a High-Order function
+function highFunction(a, b, Operation) {
+  console.log("Fazendo uma operação");
+  const valuesOperation = Operation(a, b);
+  return valuesOperation;
 }
 
-console.log(selectedCharacters);
+function sumFunction(a, b) {
+  console.log("Fazendo uma soma");
+  return a + b;
+}
 
-//Using map for the same task
-const selectedCharacters2 = characters.map(function (character) {
-  return character.name;
-});
-
-console.log(selectedCharacters2);
+console.log(highFunction(2, 3, sumFunction));

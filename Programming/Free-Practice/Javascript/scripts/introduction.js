57ced295de05fc16a93ce4083e304288dce2b680
Mario Albano <marioalbanon@gmail.com>
2023-02-10 16:58:05 -0300

    feat: added some javascript examples

---
+++ Programming/Free-Practice/Javascript/scripts/introduction.js
@@ -0,0 +1,52 @@
+//Example with var
+var userVariable = "VarType";
+//Example with let
+let userName = "Jonny";
+//Example with const
+const userComputer = "Windows";
+
+//String example
+let inputString = "This is a string";
+
+//Number example
+const inputNumber = 14;
+let PI = 3.1415;
+
+//Boolean example
+const frontEnd = true;
+let userIsAmerican = false;
+
+//Object examples
+let MarioAlbano = {
+  Name: "Mario albano",
+  Age: 27,
+  frontEnd: true,
+};
+console.log(MarioAlbano.Age);
+console.log(MarioAlbano.Name);
+console.log(MarioAlbano.frontEnd);
+
+//Function example
+function sayMyName() {
+  console.log("Mario");
+}
+
+//Undefined valor example
+let undefinedText;
+console.log(undefinedText);
+
+//Null example
+let nullText = null;
+console.log(nullText);
+
+//Operator example
+let givingValeue;
+givingValeue = "Now this variable have a value";
+let randomNumber = 1;
+randomNumber += 1;
+console.log(randomNumber);
+
+randomNumber *= 2;
+console.log(randomNumber);
+/*More Operators Example: ++, --, 0, +, %
+Comparison Operators: ==, !=, ===, !==, >, >=, <, <=*/

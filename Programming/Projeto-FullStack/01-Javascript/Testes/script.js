4c078b5620c5c71ba7d3c4c7fc7d13206cd07f36
Mario Albano <marioalbanon@gmail.com>
2023-02-10 13:44:20 -0300

    feat: hoisting test javascript

---
+++ Programming/Projeto-FullStack/01-Javascript/Testes/script.js
@@ -0,0 +1,8 @@
+//Testing a hoisting variable
+console.log(userName);
+/*
+const userName = "Batshuayi"; his will result in error
+let userName = "Batshuayi" his will result in error
+*/
+
+var userName = "Batshuayi"; //Expected undefined

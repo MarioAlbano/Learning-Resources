c52228041bb778ba124acce6a84a6ef70687d25f
Mario Albano <marioalbanon@gmail.com>
2023-02-20 00:58:53 -0300

    feat: added example with startsWith, endsWith and includes

--- Programming/React/Javascript/testes/script4.js
+++ Programming/React/Javascript/testes/script4.js
@@ -1,28 +1,32 @@
 //Map example
 const numbersOneToFive = [1, 2, 3, 4, 5];
-
-const newList = numbersOneToFive.map((item) => {
-  return item * 3;
-});
+const newList = numbersOneToFive.map((item) => item * 3);
 console.log(newList);
 
-//Index inside map
-const newList2 = numbersOneToFive.map((item, index) => {
-  return item * 3 + index;
-});
+//Index in map example
+const newList2 = numbersOneToFive.map((item, index) => item * 3 + index);
 console.log(newList2);
 
-//Reduce
-const reduceExample = numbersOneToFive.reduce((item, index) => {
-  return item + index;
-});
+//Reduce example
+const reduceExample = numbersOneToFive.reduce((item, index) => item + index);
 console.log(reduceExample);
 
-const findExample = numbersOneToFive.find((item, anotherItem) => {
-  return item === 2;
-});
+//Find example
+const findExample = numbersOneToFive.find((item) => item === 2);
 console.log(findExample);
-const findExample2 = numbersOneToFive.find((item, anotherItem) => {
-  return item === 2222;
-});
+const findExample2 = numbersOneToFive.find((item) => item === 2222);
 console.log(findExample2); //Expected undefined
+
+//Includes example
+const randomArray = ["Mario", "Hamilton", "Ronaldo"];
+console.log(randomArray.includes("Mario")); //Expected true
+console.log(randomArray.includes("Handersen")); //Expected false
+
+//startsWith example
+const randomName = "Batshuayi";
+console.log(randomName.startsWith("B")); //Expected true
+console.log(randomName.startsWith("T")); //Expected false
+
+const randomName2 = "Reus";
+console.log(randomName2.endsWith("S")); //Expected false
+console.log(randomName2.endsWith("s")); //Expected true

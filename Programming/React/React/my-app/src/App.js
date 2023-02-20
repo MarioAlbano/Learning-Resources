4ee3096ae1e584a53366ecd2e6edfbcbba74eeac
Mario Albano <marioalbanon@gmail.com>
2023-02-20 13:16:49 -0300

    feat: cleared files inside react project

--- Programming/React/React/my-app/src/App.js
+++ Programming/React/React/my-app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Olá mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+        quo dolor? Quos ducimus voluptas inventore similique voluptate, delectus
+        laborum excepturi officia rerum, commodi, animi consequuntur ipsum
+        voluptatem provident ratione odio!
+      </p>
     </div>
   );
 }
-
-export default App;

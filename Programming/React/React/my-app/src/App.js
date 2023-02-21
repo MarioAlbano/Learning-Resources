8bb0c21fe568c0de9ff7bf002ab284eb454b4179
Mario Albano <marioalbanon@gmail.com>
2023-02-21 12:41:24 -0300

    feat:added states Exercise

--- Programming/React/React/my-app/src/App.js
+++ Programming/React/React/my-app/src/App.js
@@ -1,40 +1,23 @@
 import React, { Component } from "react";
 
-class MyTeam extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myCounter: 0,
+    };
+  }
+
   render() {
     return (
       <div>
-        <h2>Este é um h2 de um componente de classe</h2>
-        <h3>Meu nome é {this.props.firstName}</h3>
-        <h3>E meu sobrenome é {this.props.forName}</h3>
-        <About firstName="John Jones" forName="Bones" />
-        <More Example="EscritoEmUmComponenteFuncional!" />
+        <h1>Este é um teste pois meu último arquivo não renderizou</h1>
+        <h2>Contador: {this.state.myCounter}</h2>
+        <button>Aumentar</button>
+        <button>Diminuir</button>
       </div>
     );
   }
 }
 
-const More = (props) => {
-  return (
-    <div>Este é um exemplo de um componente funcional {props.Example}</div>
-  );
-};
-
-class About extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Meu nome é {this.props.firstName}</h3>
-        <h3>E meu sobrenome é {this.props.forName}</h3>
-      </div>
-    );
-  }
-}
-
-export default function App() {
-  return (
-    <div>
-      <MyTeam firstName="Mario" forName="Albano" />
-    </div>
-  );
-}
+export default App;

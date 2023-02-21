c421dc07197ab5b9dd8f556c727756cb6619fb04
Mario Albano <marioalbanon@gmail.com>
2023-02-21 19:29:40 -0300

    feat: boilerplate react

--- Programming/React/React/my-app/src/App.js
+++ Programming/React/React/my-app/src/App.js
@@ -4,32 +4,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      myStatus: true,
+      
     };
-    this.desactive = this.desactive.bind(this);
-    this.active = this.active.bind(this);
   }
-  
-  desactive = () => {
-    this.setState({ myStatus: false });
-  };
-
-  active = () => {
-    this.setState({ myStatus: true });
-  };
 
   render() {
-    return (
-      <div>
-        {this.state.myStatus ? (
-          <h1>Bem vindo usuário logado</h1>
-        ) : (
-          <h1>Bem vindo usuário deslogado!</h1>
-        )}
-        <button onClick={this.desactive}>Desativar Usuário</button>
-        <button onClick={this.active}>Ativar Usuário</button>
-      </div>
-    );
+    return <div></div>;
   }
 }
 

a51c62dbbbb8b496640bce74e62d4dabb70484c6
Mario Albano <marioalbanon@gmail.com>
2023-02-21 18:29:04 -0300

    feat: added import and export component react exercise

--- Programming/React/React/my-app/src/App.js
+++ Programming/React/React/my-app/src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react";
+import Member from "./Member";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hour: "00:00:00",
-    };
-  }
-
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({ hour: new Date().toLocaleTimeString() });
-    }, 1000);
-  }
-
-  componentDidUpdate() {
-    console.log("Atualizou o State!");
-  }
-
   render() {
     return (
       <div>
-        <h1>Horário de Brasília: {this.state.hour}</h1>
+        <Member />
       </div>
     );
   }
--- Programming/React/React/my-app/src/Member/Member.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-import Member from "./Member";
-
-class Member extends Component {}
-
-export default Member;

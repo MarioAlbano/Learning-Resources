f169f785e18bb9a51714b4e379e88d053fc7abab
Mario Albano <marioalbanon@gmail.com>
2023-02-21 15:37:05 -0300

    feat: added counter react

--- Programming/React/React/my-app/src/App.js
+++ Programming/React/React/my-app/src/App.js
@@ -15,13 +15,26 @@ class App extends Component {
     this.setState(state);
   };
 
+  decrease = () => {
+    let state = this.state;
+    state.myCounter -= 1;
+    this.setState(state);
+  };
+
+  reset = () => {
+    let state = this.state;
+    state.myCounter = 0;
+    this.setState(state);
+  };
+
   render() {
     return (
       <div>
         <h1>Este é um teste pois meu último arquivo não renderizou</h1>
         <h2>Contador: {this.state.myCounter}</h2>
+        <button onClick={this.decrease}>Diminuir</button>
         <button onClick={this.increase}>Aumentar</button>
-        <button>Diminuir</button>
+        <button onClick={this.reset}>Resetar</button>
       </div>
     );
   }

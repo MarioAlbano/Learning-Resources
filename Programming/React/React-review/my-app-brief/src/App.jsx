d87e12e38e6806d2514e3353806ab84355353b0a
Mario Albano <marioalbanon@gmail.com>
2023-03-01 09:54:23 -0300

    feat: added simple props test

--- Programming/React/React-review/my-app-brief/src/App.jsx
+++ Programming/React/React-review/my-app-brief/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AppCard from "./Card";
 
+//Component need to start with Uppercase. Lowercase means a HTML for React
 function App() {
   return (
     <React.Fragment>
       <h1>It's just another test using fragment!</h1>
-      <AppCard />
+      <AppCard innerText="John" />
+      <AppCard innerText="Maria" />
+      <AppCard innerText="Heong-Ming-Son" />
     </React.Fragment>
   );
 }
--- Programming/React/React-review/my-app-brief/src/Card.jsx
+++ Programming/React/React-review/my-app-brief/src/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function AppCard() {
-  return <h1>This is a component test!</h1>;
+//We can use component to avoid copy and paste many times the same code
+function AppCard(props) {
+  return (
+    <div>
+      <h1>Hello! {props.innerText}</h1>
+    </div>
+  );
 }
 
 export default AppCard;

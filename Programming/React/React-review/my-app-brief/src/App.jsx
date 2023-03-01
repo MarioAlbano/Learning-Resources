226b049d3cf9bf9fbbcef742b9b59e5e36ac846c
Mario Albano <marioalbanon@gmail.com>
2023-03-01 09:26:29 -0300

    feat: component test

--- Programming/React/React-review/my-app-brief/src/App.jsx
+++ Programming/React/React-review/my-app-brief/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import AppCard from "./Card";
 
 function App() {
   return (
     <React.Fragment>
       <h1>It's just another test using fragment!</h1>
+      <AppCard />
     </React.Fragment>
   );
 }
--- Programming/React/React-review/my-app-brief/src/Card.jsx
+++ Programming/React/React-review/my-app-brief/src/Card.jsx
@@ -1,3 +1,7 @@
 import React from "react";
 
+function AppCard() {
+  return <h1>This is a component test!</h1>;
+}
 
+export default AppCard;

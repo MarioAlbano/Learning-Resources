6dd759bff6d8d82a5fcb86563e7d8202c6b34d2f
Mario Albano <marioalbanon@gmail.com>
2023-03-01 09:20:36 -0300

    feat: changed js files to jsx

---
+++ Programming/React/React-review/my-app-brief/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>It's just another test using fragment!</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;

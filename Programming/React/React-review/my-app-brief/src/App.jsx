7fdb3293247c030fed39278e21a5fa63f7e83300
Mario Albano <marioalbanon@gmail.com>
2023-03-01 10:04:51 -0300

    feat: added another test with props

--- Programming/React/React-review/my-app-brief/src/App.jsx
+++ Programming/React/React-review/my-app-brief/src/App.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import AppCard from "./Card";
+import NewProps from "./NewProps";
 
 //Component need to start with Uppercase. Lowercase means a HTML for React
 function App() {
   return (
     <React.Fragment>
-      <h1>It's just another test using fragment!</h1>
-      <AppCard innerText="John" />
-      <AppCard innerText="Maria" />
-      <AppCard innerText="Heong-Ming-Son" />
+      <NewProps newText="Hong-Ming-Son" />
     </React.Fragment>
   );
 }
---
+++ Programming/React/React-review/my-app-brief/src/NewProps.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewProps(props) {
+  return (
+    <React.Fragment>
+      <h1>It's just a test with props: {props.newText}</h1>
+    </React.Fragment>
+  );
+}
+
+export default NewProps;

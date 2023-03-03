09c81d0e827b679100d93b90bf602799b0aaa795
Mario Albano <marioalbanon@gmail.com>
2023-03-03 11:07:18 -0300

    feat: react hour exercise

--- Programming/React/React/my-app/src/App.js
+++ Programming/React/React/my-app/src/App.js
@@ -1,43 +1,22 @@
 import React from "react";
 
-class MyComponent extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Matheus",
-      userAge: 27,
-      counter: 0,
+      hour: "00.00.00",
     };
-    this.increase = this.increase.bind(this);
-    this.decrease = this.decrease.bind(this);
   }
 
-  increase() {
-    let state = this.state;
-    state.counter += 1;
-    this.setState(state);
-  }
-
-  decrease() {
-    let state = this.state;
-    if (state.counter !== 0) {
-      state.counter -= 1;
-      this.setState(state);
-    }
-    state.userName = "José";
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hour: new Date().toLocaleTimeString() });
+    }, 1000);
   }
 
   render() {
-    return (
-      <div>
-        <h1>Teste aqui! {this.state.userName}</h1>
-        <p>{this.state.userName}</p>
-        <p>{this.state.counter}</p>
-        <button onClick={this.decrease}> - </button>
-        <button onClick={this.increase}> + </button>
-      </div>
-    );
+    return <h1>Projeto hora em React: {this.state.hour}</h1>;
   }
 }
 
-export default MyComponent;
+export default App;

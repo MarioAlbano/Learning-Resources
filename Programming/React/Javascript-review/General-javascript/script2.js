6a4155e631caeae2f5e45b0a8c468d667ee37a95
Mario Albano <marioalbanon@gmail.com>
2023-03-20 21:21:01 -0300

    feat: changed folders and added notes about soft skills

--- Programming/React/Javascript-review/General-javascript/script2.js
+++
@@ -1,6 +0,0 @@
-if (localStorage.userName == undefined) {
-  localStorage.userName = prompt("Por favor defina seu nome");
-} else {
-  alert(`Bem vindo ${localStorage.userName}`);
-  localStorage.removeItem("userName");
-}

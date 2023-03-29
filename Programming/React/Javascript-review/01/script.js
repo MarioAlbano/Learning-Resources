179fef8ee5b95c6be4a57071c6a7d66dceac988b
Mario Albano <marioalbanon@gmail.com>
2023-03-28 21:57:57 -0300

    feat: added english grammer content

---
+++ Programming/React/Javascript-review/01/script.js
@@ -0,0 +1,30 @@
+//Variables
+const firstName = "Mario";
+console.log(firstName);
+
+let numberA = 20;
+let numberB = 10;
+const sum = numberA + numberB;
+
+console.log(sum);
+console.log(`Variável Nome: ${firstName}, Variável Soma: ${sum}`);
+
+const NOME = "Nome Maiúsculo";
+const Nome = "Nome Capitalizado";
+const nome = "Nome Minúsculo";
+
+console.log(NOME);
+console.log(Nome);
+console.log(nome);
+console.log(numberA / numberB);
+console.log(numberB % numberA);
+console.log(numberA * numberB);
+
+//Comment in Javascript = Sigle Line "//Comment Example Here" or  Multi line /*Comment Example Here*/
+
+//Output values througth prompt
+// const entryNumber = prompt("Qual o número que deseja gravar?");
+// console.log(entryNumber);
+
+//Output value througth document.write
+document.write("Esse é um teste de Output");

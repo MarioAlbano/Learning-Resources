d6381d9d782716b8c07a2df5caccc5f7376fd598
Mario Albano <marioalbanon@gmail.com>
2023-03-30 19:37:34 -0300

    feat: added info about array, array methods, switch statement

--- Programming/React/Javascript-review/01/script.js
+++ Programming/React/Javascript-review/01/script.js
@@ -21,22 +21,43 @@ console.log(numberB % numberA);
 console.log(numberA * numberB);
 
 //Comment in Javascript = Sigle Line "//Comment Example Here" or  Multi line /*Comment Example Here*/
-
 //Output values througth prompt
 // const entryNumber = prompt("Qual o número que deseja gravar?");
 // console.log(entryNumber);
-
 //Output value througth document.write
 document.write("Esse é um teste de Output");
 
-//Arrays and arrays methods Javascript
+//Arrays in Javascript
 let userInfo = ["Mario", "Albano", 27, "Single"];
 console.log(userInfo);
 
-//forEach: for each element from an array doing something
+//Arrays methods Javascript:
+
+//forEach: for each element from an array doing something (When you need a loop inside array)
 userInfo.forEach((item) => {
   console.log(item + " Info.");
 });
+
+//map: when you need to transform each element from an array
+const userAccount = [0, 1, 2, 3, 4, 5];
+const userAccountOperation = userAccount.map((item) => {
+  return item * 3;
+});
+console.log(userAccountOperation);
+
+//concat: when you need to group up two different arrays
+const firstOfFive = [0, 1, 2, 3, 4, 5];
+const lastOfTen = [6, 7, 8, 9, 10];
+const totalOfTen = firstOfFive.concat(lastOfTen);
+console.log(totalOfTen);
+
+//push: Insert in the end of array
+userInfo.push("push method here!");
+console.log(userInfo);
+
+//pop: Remove the last element from an array
+userInfo.pop();
+console.log(userInfo);
 
 //unshift: Insert on first position
 userInfo.unshift("Data Science");
@@ -46,4 +67,48 @@ console.log(userInfo);
 userInfo.shift();
 console.log(userInfo);
 
-//
+//Loops
+
+//For: Create a counter, put a limit on it, and decide if it will increase or decrease
+for (i = 0; i <= 10; i++) {
+  console.log(`${i} position`);
+}
+
+//While: You can create a loop requirements
+let userCounterExample = 0;
+let userCounterExample2 = 0;
+while (userCounterExample2 < 5) {
+  userCounterExample += 10;
+  userCounterExample2++;
+  console.log(userCounterExample);
+}
+
+//Do While: It's a different version of the For, but you need to create a counter variable
+let myCounterExample = 0;
+do {
+  myCounterExample += 5;
+  console.log(myCounterExample);
+} while (myCounterExample <= 20);
+
+//Switch: When you have a multiple choices, you can use switch statement
+switchUserOption = 3;
+
+switch (switchUserOption) {
+  case 1:
+    console.log("Option One");
+    break;
+  case 2:
+    console.log("Option Two");
+    break;
+  case 3:
+    console.log("Option Three");
+    break;
+  case 4:
+    console.log("Option Four");
+    break;
+  case 5:
+    console.log("Option Five");
+    break;
+  default:
+    console("Number not founded");
+}

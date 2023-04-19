23c9e63435f7bbea8fd70132d9dbaa6a7941902a
Mario Albano <marioalbanon@gmail.com>
2023-04-19 11:03:07 -0300

    feat: setInterval and setTimeout resume

--- Programming/React/Javascript-review/01/script3.js
+++ Programming/React/Javascript-review/01/script3.js
@@ -1,1 +1,12 @@
 //setInterval is infinite and we can create clearInterval(variableName) to stop. setTimeOut it´s only one attempt
+
+function randomInterval() {
+  document.write("<br> Um intervalo está sendo chamado");
+}
+
+let threeSeconds = setInterval(randomInterval, 3000);
+clearInterval(threeSeconds);
+
+// setTimeout(randomInterval, 1000);
+
+//webStorage save info after the user close the browser. local and session

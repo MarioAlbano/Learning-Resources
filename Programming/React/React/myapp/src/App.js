d243bb4be5bd6a1f815840f9c2feee45c940a235
Mario Albano <marioalbanon@gmail.com>
2023-04-21 20:09:22 -0300

    feat: functions with class components

--- Programming/React/React/myapp/src/App.js
+++ Programming/React/React/myapp/src/App.js
@@ -1,27 +1,10 @@
 import { Component } from "react";
-
+import Member from "./components/Member";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: "00:00:00",
-    };
-  }
-
-  //Creating a componentDidMount, using a set interval with functions to get data time
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({ date: new Date().toLocaleTimeString() });
-    }, 1000);
-  }
-
-  //Creating a componentDidUpdate and using one clg for each update that "date" have
-  componentDidUpdate() {}
-
   render() {
     return (
       <div>
-        <h1>{this.state.date}</h1>
+        <Member name="Visitor" />
       </div>
     );
   }

f6dc2ff3ad18d882a456741482f51d0d1c5ad2af
Mario Albano <marioalbanon@gmail.com>
2023-04-20 23:16:40 -0300

    feat: example with state component

--- Programming/React/React/myapp/src/App.js
+++ Programming/React/React/myapp/src/App.js
@@ -1,66 +1,53 @@
-import React from "react";
+import { Component } from "react";
 
-const MemberInfo = (props) => {
+class TeamGroup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Conheça nossa equipe:</h1>
+        <Person
+          name={this.props.name}
+          charge={this.props.charge}
+          age={this.props.age}
+          insta={this.props.insta}
+        />
+      </div>
+    );
+  }
+}
+
+class Person extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Olá eu sou o {this.props.name}</h2>
+        <h3>Cargo: {this.props.charge}</h3>
+        <h3>Idade: {this.props.age}</h3>
+        <Socials insta={this.props.insta} />
+        <hr />
+      </div>
+    );
+  }
+}
+
+class Socials extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Instagram pessoal: {this.props.insta}</h4>
+      </div>
+    );
+  }
+}
+
+//For class components, we need render()
+function App() {
   return (
     <div>
-      <MoreInfo
-        name={props.name}
-        charge={props.charge}
-        age={props.age}
-        linkedin={props.linkedin}
-        youtube={props.youtube}
-        instagram={props.instagram}
-      />
-    </div>
-  );
-};
-
-const MoreInfo = (props) => {
-  return (
-    <div>
-      <h2>Hello i'm {props.name}</h2>
-      <h3>Charge: {props.charge}</h3>
-      <h3>Age: {props.age}</h3>
-      <Socials
-        linkedin={props.linkedin}
-        youtube={props.youtube}
-        instagram={props.instagram}
-      />
-      <hr />
-    </div>
-  );
-};
-
-const Socials = (props) => {
-  return (
-    <h4>
-      Linkedin = {props.linkedin}, Youtube = {props.youtube}, Instagram ={" "}
-      {props.instagram}
-    </h4>
-  );
-};
-
-export default function App() {
-  return (
-    <div>
-      <h1>Meet our team!</h1>
-      <MemberInfo
-        name="Joseph"
-        charge="Pro Grammer"
-        age="30"
-        linkedin="1"
-        youtube="2"
-        instagram="3"
-      />
-      <MemberInfo
-        name="Letícia"
-        charge="Design"
-        age="18"
-        linkedin="1"
-        youtube="2"
-        instagram="3"
-      />
+      <TeamGroup name="José" charge="Robotista" age="45" insta="Zé_dos_robos" />
+      <TeamGroup name="Adebayor" charge="Atleta" age="28" insta="Adeebayo000" />
     </div>
   );
 }
-//Stateless component it's a simple component, dont have life cycle and dont have state
+
+export default App;

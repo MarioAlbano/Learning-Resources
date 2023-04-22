73535642a2ed1288fe16a27548fa6419400bc496
Mario Albano <marioalbanon@gmail.com>
2023-04-22 00:28:13 -0300

    feat: conditionals in react

--- Programming/React/React/myapp/src/App.js
+++ Programming/React/React/myapp/src/App.js
@@ -1,10 +1,38 @@
 import { Component } from "react";
-import Member from "./components/Member";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status1: false,
+    };
+    this.logIn = this.logIn.bind(this);
+    this.logOff = this.logOff.bind(this);
+  }
+
+  logIn() {
+    this.setState({ status1: true });
+  }
+
+  logOff() {
+    this.setState({ status1: false });
+  }
+
   render() {
     return (
       <div>
-        <Member name="Visitor" />
+        {this.state.status1 ? (
+          <div>
+            {" "}
+            <h2>Usuário logado. Bem vindo ao sistema!</h2>
+            <button onClick={this.logOff}>Clique para Deslogar</button>
+          </div>
+        ) : (
+          <div>
+            {" "}
+            <h2>Usuário deslogado. Clique no botão abaixo para Logar</h2>
+            <button onClick={this.logIn}>Clique para Logar</button>
+          </div>
+        )}
       </div>
     );
   }

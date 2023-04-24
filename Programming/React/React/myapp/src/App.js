5e40da5dfe9d4bb5e8a3b20c098c9a254dea3c76
Mario Albano <marioalbanon@gmail.com>
2023-04-24 17:39:19 -0300

    feat: tested useState, useEffect, useMemo and useCallbacl

--- Programming/React/React/myapp/src/App.js
+++ Programming/React/React/myapp/src/App.js
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 
 function App() {
   const [tasks, setTasks] = useState(["Pay bills", "Study Hooks in React"]);
 
   const [input, setInput] = useState("");
 
-  function handleAdd() {
+  //it's like componetDidUpdate or componentDidMount. Every uptade we can trigger a function
+  useEffect(() => {
+    const tasksStorage = localStorage.getItem("tasks");
+
+    if (tasksStorage) {
+      setTasks(JSON.parse(tasksStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  //Using useCallback to prevent unnecessary re-renders from functions
+  const handleAdd = useCallback(() => {
     setTasks([...tasks, input]);
     setInput("");
-  }
+  }, [tasks, input]);
+
+  //We can use useMemo to avoid unnecessary re-renders
+  const sumTasks = useMemo(() => tasks.length, [tasks]);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleAdd();
+    }
+  };
 
   return (
     <div>
@@ -21,8 +44,10 @@ function App() {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={handleKeyPress}
       />
       <button onClick={handleAdd}>Adicionar uma tarefa</button>
+      <h3>Você tem {sumTasks} tarefas!</h3>
     </div>
   );
 }

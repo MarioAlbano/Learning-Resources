d3ac814fe2e7c1bb82361dcf94b2bacdd967f031
Mario Albano <marioalbanon@gmail.com>
2023-04-28 16:50:20 -0300

    feat: added notes about typescript

--- Programming/Typescript/exercicio/notes.js
+++ Programming/Typescript/exercicio/notes.js
@@ -7,3 +7,8 @@
 //Criar o tsconfig.json: 'tsc --init' e alterar a versão. Agora para compilar é só 'tsc'. Configurar o outDir para pasta build e o rootDir como pasta src
 
 //Para testar, abrir um terminal dividido e usar 'node arquivo.js'Para não precisar sempre usar o tsc: 'tsc --watch'
+
+//Examples: let variable: string = "string". let variable: number = 15. let userId: string | number = "1213". let number2: any = 12312. let estaAutenicado: boolean = true. let numeros: number[]. let filmes: string[]. let filmes: (string|number). let aluno: [string, number]. let newUser: object {name: "jones", email:"jones@gmail.com"}. enum StatusPermission{ADMIN, USER, SUPPORT}
+
+//let statusAtual: unknown = 1.let mudaStatus: number = 0;
+//mudaStatus = statusAtual as number.

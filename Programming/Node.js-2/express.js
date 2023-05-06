5eedb77d38a1a593a4153d4a067bed93437cf2c9
Mario Albano <marioalbanon@gmail.com>
2023-05-06 19:57:29 -0300

    feat: created npm project and required express

---
+++ Programming/Node.js-2/express.js
@@ -0,0 +1,7 @@
+/*//O que é Express
+-É um framework minimalista que não define padrões de desenvolvimento
+-Opera através de middlewares
+-Padrão Chain of Responsibility
+-Entrega apenas a requisição e a resposta, deixando todo o fluxo de dados a escolha do desenvolvedor
+
+*/

d2bb64020ac678b21e4cb3ecfddf0bfd4829f7f9
Mario Albano <marioalbanon@gmail.com>
2023-05-06 12:02:20 -0300

    feat: using js in terminal

--- Programming/Node.js-2/index.js
+++ Programming/Node.js-2/index.js
@@ -3,3 +3,18 @@
 -Precisamos de uma estrutura que seja capaz de Receber e fornecer dados, validar, armazenar e assegurar consistência
 -Front faz requisição que são ouvidas por um servidor web, que passa por um código e faz interação com banco de dados
 */
+
+//Criando uma função e chamando no node
+function returnName(name) {
+  console.log(name);
+}
+returnName("Albano");
+
+//Iniciando um Package.json: 'npm init -y'. Instalando biblioteca moment: npm install moment --save
+
+const moment = require("moment"); //Importando biblioteca
+
+function returnName2(name) {
+  console.log(moment().format("HH:mm")); //Retornando a hora usando a biblioteca moment
+}
+returnName2();

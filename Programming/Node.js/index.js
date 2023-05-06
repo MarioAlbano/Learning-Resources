60884f2f591add7d490d846038d63030dc674e42
Mario Albano <marioalbanon@gmail.com>
2023-05-06 00:42:56 -0300

    feat: created API example

--- Programming/Node.js/index.js
+++ Programming/Node.js/index.js
@@ -1,3 +1,38 @@
-//Importing Express
+/*
+//JSON
+-JavaScript Objec Notation
+-São os dados estruturados
+
+//HTTP Codes
+-1xx: Informations
+-2xx: Success
+-3xx: Redirection
+-4xx: Client Error
+
+//Criando API
+-Usar 'npm init -y'
+-Instalar Express
+-Importar Express
+
+Para criar uma API:
+*/
+
+//Importando Express
 const express = require("express");
-console.log(express);
+
+//Usando uma função do express
+const server = express();
+
+//Usando uma requisição GET. Req para requisitar info, Res uma resposta pro front
+server.get("/curso", (req, res) => {
+  //Retorno no front
+  //Retornando após o =
+  const nome = req.query.nome;
+  return res.json({ curso: `Aprendendo ${nome}` });
+});
+//Ouvindo a porta 3000
+server.listen(3000);
+
+//Query params = ?nome=NodeJS
+//Route Params = /curso/2
+//Request Body = {nome: 'NodeJS', tipo:'Backend'}

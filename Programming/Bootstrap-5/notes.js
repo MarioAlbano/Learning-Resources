deff7b77b20855991787b3ccb7f8160b4ae04ff4
Mario Albano <marioalbanon@gmail.com>
2023-05-12 11:41:44 -0300

    feat: added docs and files about bootstrap 5

---
+++ Programming/Bootstrap-5/notes.js
@@ -0,0 +1,23 @@
+/*
+//Bootstrap
+-biblioteca orientada a componentes
+-layout já criados e cabe a nós alterá-las
+-Agiliza front end
+
+//Instalando
+-Copiar e colar o link e o script na documentação
+
+//Programando
+-Não precisamos gravar, mas sim conhecer os componentes
+-Consultar documentação
+-Copiar e excluir o que não usar ou criar o nosso a partir das preferências de classe
+
+//CSS
+-Muitas vezes somente o bootstrap não será o suficiente, então usaremos CSS
+-Linka abaixo do bootstrap
+-Ter atenção com os níveis de seleção. 
+
+//Breakpoints
+-São pontos específicos criados por media query para deixr responsivo
+-Padrão para a maioria dos dispositivos
+*/

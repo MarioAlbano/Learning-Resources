51a422819c69d7d2150f0e43c8dcffcc76642d52
Mario Albano <marioalbanon@gmail.com>
2023-05-13 17:43:08 -0300

    feat: added fundamentals about bootstrap 5

---
+++ Programming/Bootstrap-5/fundamentos/notes.js
@@ -0,0 +1,67 @@
+/*
+//Bootstrap
+-biblioteca orientada a componentes
+-layout já criados e cabe a nós alterá-las
+-Agiliza front end
+
+//Instalando
+-Documentação
+
+//Programando
+-Não precisa decorar
+-Consultar documentação
+-Copiar e excluir partes ou criar
+
+//CSS
+-As vezes precisamos do CSS
+-Link abaixo do bootstrap
+-Atenção com os níveis de seleção. 
+
+//Breakpoints
+-Pontos específicos para telas, responsivo
+-cm, md, lg
+
+//Containers
+-Separar o site em áreas
+
+//Container fluid
+-Utiliza uma área maior, 100% da área útil
+-Elimina margens
+
+//Container por breakpont
+-container-sm, container-md, container-lg
+
+//Grid
+-colocar .row  na div e nos itens inserir .col
+
+//Tamanho das colunas
+-Podemos ajudar as colunas, ex: col-3
+-No máximo 12 divisões
+
+//Limitando as col
+-limitar as col em uma row
+-classe: row-cols-*, * é o limite
+
+//Alinhamento vertical
+-temos classes com flexbox, ex: align-items-end
+
+//Alinhamento horizontal
+-Usar dentro do elemento pai row
+-justify-content-center
+
+//Alinhamento unitário
+-align-self-center
+-Somente vertical
+
+//Ordem dos itens
+-order-*
+-Ex: order-3
+
+//Margem por colunas
+-Como se fosse uma coluna vazia a esquerda para separar elementos
+-offset-md-3
+
+//Gutters
+-Intervalos entre as colunas
+-Ex: gx-2, gy-4
+*/

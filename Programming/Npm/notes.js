74fa0e0e23bcf976427222870b6bda5d0dc72397
Mario Albano <marioalbanon@gmail.com>
2023-05-14 13:57:50 -0300

    feat: created npm folder and added notes and examples

---
+++ Programming/Npm/notes.js
@@ -0,0 +1,12 @@
+/*//NPM
+-As vezes ocorre conflito por causa das versões
+-Verificar versão: npm -v
+
+
+//Criando projeto
+Criar: npm init
+Criar rápido: npm init --yes
+
+//Configurar PACKAGE.json
+-Alterando: npm config set ini-author-name Albano
+*/

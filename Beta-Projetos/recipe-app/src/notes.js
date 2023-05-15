4df170f1587133e30cb5610ceb684c18e66da4ae
Mario Albano <marioalbanon@gmail.com>
2023-05-15 12:23:42 -0300

    feat: setup project with react and express

---
+++ Beta-Projetos/recipe-app/src/notes.js
@@ -0,0 +1,7 @@
+/*
+-Instalar react 'create-react-app nome_app'
+-limpar projeto
+-cd nome_app, npm start
+-instalar: express, express-handlebars, body-parser, sequelize sqlite3
+-instalar nodemon: nom install -D nodemon
+*/

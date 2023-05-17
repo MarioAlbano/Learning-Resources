a3f75c02e0ad068b358a52e87088d1d28d6e8e2c
Mario Albano <marioalbanon@gmail.com>
2023-05-17 12:27:08 -0300

    build: added folders for fetch api and mongodb

--- Programming/MongoDB/notes.js
+++ Programming/MongoDB/notes.js
@@ -1,28 +1,4 @@
-/*
-//Relacional x Não relacional
--Relacional demandam forte configuração
--Não relacionais não são rigorosos
--Relacional: Flexibilidade / Não Relacional: Desorganização
+/*//Verificar todos os bancos
+-show dbs
 
-//MongoDB
--Bando não relacional mais utilizado
--Dados inseridos em formao de objeto (JSON)
--Comando em métodos, podemos criar relações entre entdades, adaptável e é próximo do javascript
-
-//Instalação
--MongoDB
--MongoDB tools
-
-//Principais entidades
--Database: onde fica collections e dados
--Collections: são como tabelas, serve para inserir os dados
--Documents: São os dados
--Collections podem ser criadas livremente a qualquer momento e não possuem colunas fixas para os dados
-
-//Criando um banco de dados
--usar 'mongosh' no terminal
--usar 'show dbs' para ver os bancos
--usar 'use primeirobanco' para criar um banco
--Criar collection: 'db.primeiracollection.intertOne({ nome: "Mario", idade: 30})'
--Para achar o dado: db.primeiracollection.findOne({})
  */

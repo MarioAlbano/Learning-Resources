83c5ad7a0cfac27601db48f2c2d6894fe28ff042
Mario Albano <marioalbanon@gmail.com>
2023-05-18 15:42:19 -0300

    feat: added notes and examples in SQL

---
+++ Programming/SQL/myfile.sql
@@ -0,0 +1,11 @@
+ALTER TABLE empregados ADD COLUMN colunaTeste VARCHAR(50);
+ALTER TABLE empregados MODIFY COLUMN colunaTeste INT;
+SELECT * FROM empregados;
+UPDATE empregados SET colunaTeste = 12345 WHERE id= 01;
+INSERT INTO empregados VALUES (2,'Joshua', 34, 'Estudante', '2020-12-12 23:01:02', 2222);
+SHOW columns FROM empregados;
+SELECT * from empregados WHERE id = 1;
+UPDATE empregados SET colunaTeste = 9999 WHERE id = 01;
+INSERT INTO empregados (iD, nome, idade, profissao) VALUES(4,'John', 30, 'Futuro Dev');
+DELETE FROM empregados WHERE id = 4;
+SELECT * FROM empregados;

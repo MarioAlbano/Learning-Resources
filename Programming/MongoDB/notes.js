6b539b79652ad903014b3c2835df6d78f8e2ca7f
Mario Albano <marioalbanon@gmail.com>
2023-05-24 18:34:56 -0300

    feat: added notes mongodb

--- Programming/MongoDB/notes.js
+++ Programming/MongoDB/notes.js
@@ -1,4 +1,43 @@
-/*//Verificar todos os bancos
--show dbs
+/*//MongoDB
+-Banco de dados não estruturado
+-Flexibilidade, escalabilidade horizontal, performance
+-document: objects, collection: group of documents, database: group of collections
 
+//Installing
+-MongoDbcompass
+-Mongoshell
+-add mongosh in Environment variables
+-install mongodb in vscode, click to connect in extensions tab
+-right click and "launch mongodb shell"
+-now we can use 'mongosh' to start mongodb
+
+//databases
+-show dbs
+-use name_database
+-db.createCollection("")
+-db.dropDatabase()
+-we can use mongodbcompass for it
+
+//inserting
+-one document: db.name_collection.insertOne({})
+-more documents: db.name_collection.insertMany([{},{}])
+-finding: db.name_collection.find()
+-we can use mongodbcompass for it
+
+//Data type
+-String, number, boolean, ObjectId, date, array, embedded document, null, regular expression, binary data
+
+//Sorting and limiting
+-db.name_collection.find().sort({})
+-db.name_collection.find().limit()
+-Can do the same in "more options" in mongodbcompass
+
+//find
+-db.name_collection.find({filter})
+-db.name_collection.find({filter, another_filter})
+-db.name_collection.find({}, {name:true, _id: false})
+
+
+//update
+-db.students.updateOne({})
  */

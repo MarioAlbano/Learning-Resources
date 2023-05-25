4d1e02c84824a83a303b17e2decf4927787f0908
Mario Albano <marioalbanon@gmail.com>
2023-05-25 18:27:13 -0300

    feat: added new javascript resume file

---
+++ Programming/Javascript/Javascript-resume/resume-01.js
@@ -0,0 +1,23 @@
+/*//Javascript
+-Linguagem interpretada
+-ECMAScript
+-Navegador(cliente) e servidor(node)
+-Padrão dos navegadores
+-Frameworks
+-Fullstack
+-Mobile, Desktop
+
+//Fundamentos
+-Variáveis e tipos de dados
+-Listas (arrays)
+-Objetos
+-JSON
+-Loops(for, for of, while, forEach)
+-Condicionais (if,else,switch, ternary)
+-Funções e arrow functions
+-Programação Orientada a Objetos
+-Selecionar elementos do DOM
+-Manipular elementos do DOM
+-Eventos
+-Criar e validar um formulário
+ */

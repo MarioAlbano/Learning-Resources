de6f1a16a5476991593493bf6a555e0426ee4c34
Mario Albano <marioalbanon@gmail.com>
2023-06-05 11:23:04 -0300

    feat: created node resume file

---
+++ Programming/Node/Node-resume/resume.js
@@ -0,0 +1,12 @@
+/*Fundamentos
+-Fundamentos do Javascript (objetos, arrays, condicionais etc.)
+-JSON
+-HTTP (status code, headers)
+*/
+
+/*Node.js
+-Execução do Javascript fora do navegador
+-V8
+-Constroi APIs
+-Single threaded, non-blocking (várias operações simultâneas)
+*/

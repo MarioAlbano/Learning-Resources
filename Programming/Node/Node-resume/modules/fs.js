2e5fc7cf2be844b22aa34d79bfa192151238b6e5
Mario Albano <marioalbanon@gmail.com>
2023-06-05 17:01:26 -0300

    feat: examples creating folder, content, changing content, reading folder in node

--- Programming/Node/Node-resume/modules/fs.js
+++ Programming/Node/Node-resume/modules/fs.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 fs.mkdir("pastaCriadaFs", (err) => {
   if (err) {
@@ -7,3 +8,42 @@ fs.mkdir("pastaCriadaFs", (err) => {
   }
   console.log("Diretório criado com sucesso!");
 });
+
+fs.writeFile(
+  path.join(__dirname, "teste.html"),
+  "Olá mundo! Estou criando este texto com o Node!",
+  "utf8",
+  (err) => {
+    if (err) {
+      console.log("Algo deu errado!");
+      return;
+    }
+    console.log("Arquivo criado com sucesso!");
+  }
+);
+
+setTimeout(() => {
+  fs.appendFile(
+    path.join(__dirname, "teste.html"),
+    "Olá mundo! Estou alterando um arquivo já criado com o Node!",
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error("Erro ao alterar arquivo!");
+        return;
+      }
+      console.log("Arquivo alterado com sucesso!");
+    }
+  );
+}, 1500);
+
+setTimeout(() => {
+  fs.readFile(path.join(__dirname, "teste.html"), "utf8", (err, data) => {
+    if (err) {
+      console.log("Erro ao ler aquivo");
+      return;
+    }
+    console.log("Arquivo identificado! Fazendo a leitura...");
+    console.log(data);
+  });
+}, 2000);

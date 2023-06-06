69faedaa8aad67a943abc23b2178a37f8ae09292
Mario Albano <marioalbanon@gmail.com>
2023-06-06 20:23:56 -0300

    feat: added paths in server

--- Programming/Node/Node-resume/modules/server.js
+++ Programming/Node/Node-resume/modules/server.js
@@ -6,10 +6,21 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   const analiseUrl = url.parse(req.url, true);
   const caminho = analiseUrl.pathname;
+  const mensagemJSON = {
+    mensagem: "Criado arquivo JSON com o Node.js",
+    horario: new Date(),
+  };
 
   if (caminho === "/bem-vindo") {
     res.setHeader("Content-Type", "text/plain");
     res.end("Seja bem vindo!");
+  } else if (caminho === "/sobre") {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(mensagemJSON));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.end("Página não encontrada. Tente novamente!");
   }
 });
 

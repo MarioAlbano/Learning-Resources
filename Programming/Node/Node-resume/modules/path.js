8121910678a124558d4ebb849c34bd88bf18af96
Mario Albano <marioalbanon@gmail.com>
2023-06-26 16:50:43 -0300

    feat: added more notes about node modules

--- Programming/Node/Node-resume/modules/path.js
+++ Programming/Node/Node-resume/modules/path.js
@@ -1,3 +1,15 @@
 const path = require("path");
 
-const filePath = "";
+//Apenas o nome do arquivo
+const fileName = path.basename(__filename);
+console.log(fileName);
+
+//Diretório atual
+const fileFolder = path.basename(__dirname);
+console.log(fileFolder);
+
+//Extensão do arquivo
+//Criar objeto Path
+//Juntar caminhos de arquivos
+
+//No fs -> Criar uma pasta com promisse

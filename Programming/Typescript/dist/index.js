6f75ec2c85c3f9ac59f5297bd9bb05a88de06554
Mario Albano <marioalbanon@gmail.com>
2023-07-02 17:28:46 -0300

    feat: added example with types in typescript

--- Programming/Typescript/dist/index.js
+++ Programming/Typescript/dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
-let age = 18;
-console.log(age);
+const myAge = 28;
+console.log(myAge);
+const mySurname = "Albano";
+let lastName = "Navas";
+// lastName = 14; Esperado Erro
+const isTrue = true;
+let userInput = "";
+userInput = 10;
+userInput = "10";
+let myArray = ["A", "B", "C", "D"];
+console.log(myArray);
+//Todo: vários tipos de array
--- Programming/Typescript/src/index.ts
+++ Programming/Typescript/src/index.ts
@@ -1,3 +1,15 @@
-let age: number = 18;
+const myAge: number = 28;
+console.log(myAge);
 
-console.log(age);
+const mySurname: string = "Albano";
+let lastName = "Navas";
+// lastName = 14; Esperado Erro
+
+const isTrue: boolean = true;
+let userInput: any = "";
+userInput = 10;
+userInput = "10";
+
+let myArray: string[] = ["A", "B", "C", "D"];
+console.log(myArray);
+//Todo: vários tipos de array

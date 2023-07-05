d73dc034956daad14243b4c7ca05c069b48b14e2
Mario Albano <marioalbanon@gmail.com>
2023-07-04 23:16:40 -0300

    feat: added comments in code, added examples in typescript

--- Programming/Typescript/dist/index.js
+++ Programming/Typescript/dist/index.js
@@ -1,13 +1,45 @@
 "use strict";
+//Number exemplo
 const myAge = 28;
 console.log(myAge);
+//String exemplo
 const mySurname = "Albano";
 let lastName = "Navas";
 // lastName = 14; Esperado Erro
+//Boolean exemplo
 const isTrue = true;
 let userInput = "";
 userInput = 10;
 userInput = "10";
+//Array exemplo
 let myArray = ["A", "B", "C", "D"];
 console.log(myArray);
-//Todo: vários tipos de array
+//Tupla exemplo
+let tuplaTest;
+tuplaTest = ["Teste de tupla", 200, true];
+//Lista de tuplas exemplo
+let tuplaListTest;
+tuplaListTest = [];
+tuplaListTest.push(["Tupla_01", 10]);
+console.log(tuplaListTest);
+tuplaListTest.push(["Tupla_01", 10]);
+//Intersections exemplo
+const interExample = 10;
+const interExample2 = "10";
+//Enum exemplo
+var Direction;
+(function (Direction) {
+    Direction[Direction["cima"] = 0] = "cima";
+    Direction[Direction["baixo"] = 1] = "baixo";
+    Direction[Direction["esquerda"] = 2] = "esquerda";
+    Direction[Direction["direita"] = 3] = "direita";
+})(Direction || (Direction = {}));
+const enumZero = Direction.cima;
+console.log(enumZero);
+const enumTres = Direction.direita;
+console.log(enumTres);
+//Type Assertions exemplo
+let typeExample = "Teste de Type aqui";
+let typeExample2 = typeExample.length;
+console.log(typeExample2);
+//Todo: Type Assertions, funções com string;void, object type, uso do ?, lista de objeto
--- Programming/Typescript/src/index.ts
+++ Programming/Typescript/src/index.ts
@@ -1,16 +1,53 @@
+//Number exemplo
 const myAge: number = 28;
 console.log(myAge);
 
+//String exemplo
 const mySurname: string = "Albano";
 let lastName = "Navas";
 // lastName = 14; Esperado Erro
 
+//Boolean exemplo
 const isTrue: boolean = true;
 let userInput: any = "";
 userInput = 10;
 userInput = "10";
 
+//Array exemplo
 let myArray: string[] = ["A", "B", "C", "D"];
 console.log(myArray);
 
-//Todo: tupla, lista de tupla, Intersections, Enum, Type Assertions, funções com string;void, object type, uso do ?, lista de objeto
+//Tupla exemplo
+let tuplaTest: [string, number, boolean];
+tuplaTest = ["Teste de tupla", 200, true];
+
+//Lista de tuplas exemplo
+let tuplaListTest: [string, number][];
+tuplaListTest = [];
+tuplaListTest.push(["Tupla_01", 10]);
+console.log(tuplaListTest);
+tuplaListTest.push(["Tupla_01", 10]);
+
+//Intersections exemplo
+const interExample: string | number = 10;
+const interExample2: string | number = "10";
+
+//Enum exemplo
+enum Direction {
+  cima,
+  baixo,
+  esquerda,
+  direita,
+}
+
+const enumZero: Direction = Direction.cima;
+console.log(enumZero);
+const enumTres: Direction = Direction.direita;
+console.log(enumTres);
+
+//Type Assertions exemplo
+let typeExample: any = "Teste de Type aqui";
+let typeExample2: number = (typeExample as string).length;
+console.log(typeExample2);
+
+//Todo: Type Assertions, funções com string;void, object type, uso do ?, lista de objeto

fb71e00a4be35465dafbdfffee7f68928bcb1101
Mario Albano <marioalbanon@gmail.com>
2023-07-11 10:35:36 -0300

    feat: example useState in react

--- Programming/React/react-app/src/App.js
+++ Programming/React/react-app/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const message = "Hello World!";
-  return <div className="container">{message}</div>;
+  let [message, newMessage] = useState("Esta é uma mensagem comum");
+
+  return (
+    <div className="container">
+      {message}
+      <button
+        onClick={() => {
+          newMessage("Este é um texto feito por useState");
+        }}
+      >
+        Clique em mim!
+      </button>
+    </div>
+  );
 };
 
 export default App;

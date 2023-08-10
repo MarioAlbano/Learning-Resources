e837f96757a023b9652d0661ffa92f9eab2d9c40
Mario Albano <marioalbanon@gmail.com>
2023-08-10 14:25:04 -0300

    feat: used route params example in node.js

--- Programming/Node-2/express.js
+++ Programming/Node-2/express.js
@@ -1,10 +1,22 @@
 const express = require("express"); //Importing express
 const app = express(); //Using instance
 const PORT = 3030; //Creating port
-app.listen(PORT, () => {
-  console.log("Server funcionando");
-});
 
 app.get("/", (req, res) => {
   res.send("Testando o express");
 });
+
+app.get("/:id", (req, res) => {
+  res.send(`Route Params exemplo ${req.params.id}`);
+});
+
+app.post("/user/", (req, res) => {
+  const data = "João Felix";
+  res.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log("Server funcionando");
+});
+
+//Novo endpoint criar e deletar,

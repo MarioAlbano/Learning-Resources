4a1fbb9912823c302985dabf8e9512f4281b4c1b
Mario Albano <marioalbanon@gmail.com>
2023-08-14 10:44:12 -0300

    feat: added sort, some, every and flatMap methods example

--- Programming/Node-2/javascript-resume/array/array-resume.js
+++ Programming/Node-2/javascript-resume/array/array-resume.js
@@ -35,5 +35,44 @@ const sumNumbers = myNumbers.reduce((accumulator, currentValue) => {
 }, 5);
 console.log(sumNumbers);
 
-//Usando keys no node
-const funcao
+//keys example
+const myObject = {
+  objectName: "Name-01",
+  age: 20,
+  occupation: "Developer",
+};
+
+const keysExample = Object.keys(myObject);
+console.log(keysExample);
+
+//sort example
+const sortExample = myNumbers.sort((a, b) => {
+  return a - b;
+});
+
+const numberExample2 = [14, 22, 2, 55, 3, 7];
+const sortExample2 = numberExample2.sort((a, b) => {
+  return b - a;
+});
+
+console.log(sortExample);
+console.log(sortExample2);
+
+//some and every example
+const numberExample3 = [10, 25, 5, 40, 15];
+const someExample = numberExample3.some((item) => item > 30);
+console.log(someExample);
+
+const everyExample = numberExample3.every((item) => item > 5);
+console.log(everyExample);
+
+//flatMap example
+const slittedArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8],
+];
+
+const flatMapExample = slittedArray.flatMap((item) => item);
+console.log(flatMapExample);

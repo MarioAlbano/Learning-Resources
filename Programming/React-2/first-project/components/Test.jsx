1588bdb66bc033b99087d86fcf64173f03c8e819
Mario Albano <marioalbanon@gmail.com>
2023-08-14 11:56:01 -0300

    feat: added components with typescript in react

---
+++ Programming/React-2/first-project/components/Test.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div>
+      <h2>Este é um componente reutilizável</h2>
+    </div>
+  );
+}
--- Programming/React-2/first-project/src/App.tsx
+++ Programming/React-2/first-project/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Teste</h1>
+      <Student name={"Mario"} age={28} />
+      <Student name={"Edgar"} age={18} />
+      <Student name={"Edmilson"} age={50} />
+    </div>
+  );
 }
 
-export default App
+interface AlunoProps {
+  name: string;
+  age: number;
+}
+
+function Student({ name, age }: AlunoProps) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h3>{age}</h3>
+    </div>
+  );
+}

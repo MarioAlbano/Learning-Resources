dda04a43bef4b7e01162afc85e9271ca458020e1
Mario Albano <marioalbanon@gmail.com>
2023-10-02 09:35:22 -0300

    feat: added content in java

--- Programming/Java/basicoJava/src/Main.java
+++ Programming/Java/basicoJava/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int intExample = 50;
-        double doubleExample = 9.99;
-        boolean booleanExample = true;
-        char charExample = 'B';
-        String StringExample = "This is a string Example";
+        int variableX = 123;
+        int variableY = 1000;
+
+        System.out.println(variableX - 3);
+        System.out.printf("Variável X = %d, Vaviável Y = %d%n", variableX,variableY);
     }
 }

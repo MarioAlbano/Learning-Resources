fd84a418a6f033c4886d6124083947367bdd0893
Mario Albano <marioalbanon@gmail.com>
2023-10-04 15:51:55 -0300

    feat: created new files for SQL project

--- Programming/Java/basicoJava/src/Main.java
+++ Programming/Java/basicoJava/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int variableX = 123;
-        int variableY = 1000;
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter with you name, please: ");
+        String userInput = scanner.nextLine();
+        System.out.println("Please, now enter with your age");
+        int userInput2 = scanner.nextInt();
 
-        System.out.println(variableX - 3);
-        System.out.printf("Variável X = %d, Vaviável Y = %d%n", variableX,variableY);
+
+        System.out.printf(String.format("My name is %s, and I am %d years old", userInput, userInput2));
+
+        scanner.close();
     }
 }
